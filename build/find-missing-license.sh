c37405ce10b9bc4985b1df58d344e01f9262e8cd
Ivan Zhang <ivan@ivanzhang.ca>
2019-02-21 11:45:19 -0500

    typo

--- build/find-missing-license.sh
+++ build/find-missing-license.sh
@@ -19,7 +19,7 @@ output=$(find . -type f \
 ! -path "./examples/*" \
 ! -path "./dev/config/*" \
 ! -path "./bin/*" \
-! -path "./circleci/*" \
+! -path "./.circleci/*" \
 ! -path "./.git/*" \
 ! -name LICENSE \
 ! -name requirements.txt \
