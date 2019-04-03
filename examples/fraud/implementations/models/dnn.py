657f2bc28d0347fba5c3c0ea9a90c2bc2f1fd6f4
Omer Spillinger <ospillinger@users.noreply.github.com>
2019-04-03 16:53:28 -0700

    Update examples

--- examples/fraud/implementations/models/dnn.py
+++ examples/fraud/implementations/models/dnn.py
@@ -11,5 +11,6 @@ def create_estimator(run_config, model_config):
         feature_columns=feature_columns,
         hidden_units=model_config["hparams"]["hidden_units"],
         n_classes=2,
+        weight_column="class_weight",
         config=run_config,
     )
