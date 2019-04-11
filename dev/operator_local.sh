08d4a1011f2d4e47a6275e01c2f224caf262fc45
Ivan Zhang <ivan@ivanzhang.ca>
2019-04-11 18:58:28 -0400

    clean up olocal

--- dev/operator_local.sh
+++ dev/operator_local.sh
@@ -25,8 +25,5 @@ export CONST_OPERATOR_TRANSFORMERS_DIR=$ROOT/pkg/transformers
 export CONST_OPERATOR_AGGREGATORS_DIR=$ROOT/pkg/aggregators
 export CONST_OPERATOR_IN_CLUSTER=false
 
-if ! command -v rerun >/dev/null; then
-  GO111MODULE=off go get -u -v github.com/VojtechVitek/rerun/cmd/rerun
-fi
 rerun -watch $ROOT/pkg -ignore $ROOT/vendor $ROOT/bin -run sh -c "go build -o $ROOT/bin/operator $ROOT/pkg/operator && $ROOT/bin/operator"
 # go run -race $ROOT/pkg/operator/operator.go
