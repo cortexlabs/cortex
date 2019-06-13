6f0b0cf492609e95325a9cbfa2a4ae268a6ea0d9
vishal <vishalbollu@users.noreply.github.com>
2019-06-13 20:52:00 +0000

    Update round 2

--- examples/movie-ratings/implementations/models/basic_embedding.py
+++ examples/movie-ratings/implementations/models/basic_embedding.py
@@ -2,26 +2,18 @@ import tensorflow as tf
 
 
 def create_estimator(run_config, model_config):
-    user_id_index = model_config["aggregates"]["user_id_index"]
-    movie_id_index = model_config["aggregates"]["movie_id_index"]
-
-    feature_columns = [
-        tf.feature_column.embedding_column(
+    embedding_feature_columns = []
+    for feature_col_data in model_config["input"]["embedding_columns"]:
+        embedding_col = tf.feature_column.embedding_column(
             tf.feature_column.categorical_column_with_identity(
-                "user_id_indexed", len(user_id_index)
+                feature_col_data["col"], len(feature_col_data["vocab"]["index"])
             ),
             model_config["hparams"]["embedding_size"],
-        ),
-        tf.feature_column.embedding_column(
-            tf.feature_column.categorical_column_with_identity(
-                "movie_id_indexed", len(movie_id_index)
-            ),
-            model_config["hparams"]["embedding_size"],
-        ),
-    ]
+        )
+        embedding_feature_columns.append(embedding_col)
 
     return tf.estimator.DNNRegressor(
-        feature_columns=feature_columns,
+        feature_columns=embedding_feature_columns,
         hidden_units=model_config["hparams"]["hidden_units"],
         config=run_config,
     )
--- examples/reviews/implementations/transformers/tokenize_string_to_int.py
+++ examples/reviews/implementations/transformers/tokenize_string_to_int.py
@@ -4,18 +4,19 @@ non_word = re.compile("\\W")
 
 
 def transform_python(input):
-    text = input["col"].lower()
+    lol = input
+    text = lol["col"].lower()
     token_index_list = []
-    vocab = input["vocab"]
+    vocab = lol["vocab"]
 
     for token in non_word.split(text):
         if len(token) == 0:
             continue
         token_index_list.append(vocab.get(token, vocab["<UNKNOWN>"]))
-        if len(token_index_list) == input["max_len"]:
+        if len(token_index_list) == lol["max_len"]:
             break
 
-    for i in range(input["max_len"] - len(token_index_list)):
+    for i in range(lol["max_len"] - len(token_index_list)):
         token_index_list.append(vocab["<PAD>"])
 
     return token_index_list
