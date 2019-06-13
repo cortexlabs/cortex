6071f4e01f48c6d104fc833dadd13bbd33254b14
vishal <vishalbollu@users.noreply.github.com>
2019-06-13 20:56:55 +0000

    Add data yaml files

--- examples/reviews/implementations/transformers/tokenize_string_to_int.py
+++ examples/reviews/implementations/transformers/tokenize_string_to_int.py
@@ -4,19 +4,18 @@ non_word = re.compile("\\W")
 
 
 def transform_python(input):
-    lol = input
-    text = lol["col"].lower()
+    text = input["col"].lower()
     token_index_list = []
-    vocab = lol["vocab"]
+    vocab = input["vocab"]
 
     for token in non_word.split(text):
         if len(token) == 0:
             continue
         token_index_list.append(vocab.get(token, vocab["<UNKNOWN>"]))
-        if len(token_index_list) == lol["max_len"]:
+        if len(token_index_list) == input["max_len"]:
             break
 
-    for i in range(lol["max_len"] - len(token_index_list)):
+    for i in range(input["max_len"] - len(token_index_list)):
         token_index_list.append(vocab["<PAD>"])
 
     return token_index_list
