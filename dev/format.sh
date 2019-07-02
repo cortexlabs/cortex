3068dee02ad87773e3dc6741dd006704fc0c1736
Omer Spillinger <ospillinger@users.noreply.github.com>
2019-07-02 11:02:12 -0700

    Add link to YAML source

--- dev/format.sh
+++ dev/format.sh
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 set -euo pipefail
 
 ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")"/.. >/dev/null && pwd)"
