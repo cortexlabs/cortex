dc1832f6a9053f2ffd8e1bc84cd8cdf48c15348c
David Eliahu <deliahu@users.noreply.github.com>
2019-07-05 16:53:31 -0700

    Remove trailing whitespace

--- pkg/workloads/tf_api/api.py
+++ pkg/workloads/tf_api/api.py
@@ -394,10 +394,10 @@ def predict(deployment_name, api_name):
 def validate_model_dir(model_dir):
     """
     validates that model_dir has the expected directory tree.
-    
+
     For example (your TF serving version number may be different):
 
-    1562353043/   
+    1562353043/
         saved_model.pb
         variables/
             variables.data-00000-of-00001
