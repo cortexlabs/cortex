4525779e62a4940eb63583fbfcc54ba1dcfe0f8d
David Eliahu <deliahu@users.noreply.github.com>
2019-08-07 12:40:46 -0700

    Hide istio installation logs

--- manager/install_cortex.sh
+++ manager/install_cortex.sh
@@ -106,7 +106,7 @@ function setup_istio() {
     {\"op\": \"add\", \"path\": \"/spec/template/spec/containers/0/env/-\", \"value\": {\"name\": \"AWS_ACCESS_KEY_ID\", \"valueFrom\": {\"secretKeyRef\": {\"name\": \"aws-credentials\", \"key\": \"AWS_ACCESS_KEY_ID\"}}}},\
     {\"op\": \"add\", \"path\": \"/spec/template/spec/containers/0/env/-\", \"value\": {\"name\": \"AWS_SECRET_ACCESS_KEY\", \"valueFrom\": {\"secretKeyRef\": {\"name\": \"aws-credentials\", \"key\": \"AWS_SECRET_ACCESS_KEY\"}}}},\
   ]"
-  kubectl patch deployment istio-telemetry -n istio-system --type='json' -p="$istio_patch"
+  kubectl patch deployment istio-telemetry -n istio-system --type='json' -p="$istio_patch" >/dev/null
 }
 
 function validate_cortex() {
