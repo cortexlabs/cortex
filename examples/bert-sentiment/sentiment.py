478ae50c2c21f40ff5cdd307f1171716de1d8c66
Ivan Zhang <ivan@ivanzhang.ca>
2019-08-08 11:16:09 -0400

    format

--- examples/bert-sentiment/sentiment.py
+++ examples/bert-sentiment/sentiment.py
@@ -12,23 +12,27 @@ labels = ["Negative", "Positive"]
 BERT_MODEL_HUB = "https://tfhub.dev/google/bert_uncased_L-12_H-768_A-12/1"
 max_seq_length = 128
 
+
 def create_tokenizer_from_hub_module():
     """Get the vocab file and casing info from the Hub module."""
     with tf.Graph().as_default():
         bert_module = hub.Module(BERT_MODEL_HUB)
         tokenization_info = bert_module(signature="tokenization_info", as_dict=True)
         with tf.Session() as sess:
-            vocab_file, do_lower_case = sess.run([tokenization_info["vocab_file"],
-                                            tokenization_info["do_lower_case"]])
+            vocab_file, do_lower_case = sess.run(
+                [tokenization_info["vocab_file"], tokenization_info["do_lower_case"]]
+            )
 
-    return bert.tokenization.FullTokenizer(
-      vocab_file=vocab_file, do_lower_case=do_lower_case)
+    return bert.tokenization.FullTokenizer(vocab_file=vocab_file, do_lower_case=do_lower_case)
+
 
 tokenizer = create_tokenizer_from_hub_module()
+
+
 def pre_inference(sample, metadata):
     tokens_a = tokenizer.tokenize(sample["input"][0])
     if len(tokens_a) > max_seq_length - 2:
-      tokens_a = tokens_a[0:(max_seq_length - 2)]
+        tokens_a = tokens_a[0 : (max_seq_length - 2)]
     tokens = []
     tokens.append("[CLS]")
     for token in tokens_a:
@@ -40,9 +44,7 @@ def pre_inference(sample, metadata):
     while len(input_ids) < max_seq_length:
         input_ids.append(0)
 
-    return {
-        "input": input_ids
-    }
+    return {"input": input_ids}
 
 
 def post_inference(prediction, metadata):
