6e6563f1dab11bd9866d56293923aedb42594ba3
Ivan Zhang <ivan@ivanzhang.ca>
2019-08-09 23:24:52 -0400

    clean up example

--- examples/sentiment/sentiment.py
+++ examples/sentiment/sentiment.py
@@ -5,16 +5,11 @@ import bert
 from bert import run_classifier
 from bert import tokenization
 
-labels = ["Negative", "Positive"]
-
-BERT_MODEL_HUB = "https://tfhub.dev/google/bert_uncased_L-12_H-768_A-12/1"
-max_seq_length = 128
-
 
 def create_tokenizer_from_hub_module():
     """Get the vocab file and casing info from the Hub module."""
     with tf.Graph().as_default():
-        bert_module = hub.Module(BERT_MODEL_HUB)
+        bert_module = hub.Module("https://tfhub.dev/google/bert_uncased_L-12_H-768_A-12/1")
         tokenization_info = bert_module(signature="tokenization_info", as_dict=True)
         with tf.Session() as sess:
             vocab_file, do_lower_case = sess.run(
@@ -25,16 +20,18 @@ def create_tokenizer_from_hub_module():
 
 
 tokenizer = create_tokenizer_from_hub_module()
+labels = ["Negative", "Positive"]
 
 
 def pre_inference(sample, metadata):
-    input_examples = [
-        run_classifier.InputExample(guid="", text_a=sample["input"][0], text_b=None, label=0)
-    ]
-    input_features = bert.run_classifier.convert_examples_to_features(
-        input_examples, [0, 1], 128, tokenizer
+    input_feature = run_classifier.convert_single_example(
+        0,  # example Idx
+        run_classifier.InputExample(guid="", text_a=sample["input"], text_b=None, label=0),
+        [0, 1],  # label IDs
+        128,  # max len
+        tokenizer,
     )
-    return {"input_ids": input_features[0].input_ids}
+    return {"input_ids": input_feature.input_ids}
 
 
 def post_inference(prediction, metadata):
