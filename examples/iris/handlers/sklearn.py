974272cdc849b11bfbb2896ed740d9a8c7fa6ae7
David Eliahu <deliahu@users.noreply.github.com>
2019-08-15 14:00:52 -0700

    Add example of using scalars (#337)

--- examples/iris/handlers/sklearn.py
+++ examples/iris/handlers/sklearn.py
@@ -1,15 +1,29 @@
 import numpy as np
+import boto3
+import json
+
+from cortex.lib.log import get_logger
+
+logger = get_logger()
+s3 = boto3.client("s3")
 
 iris_labels = ["Iris-setosa", "Iris-versicolor", "Iris-virginica"]
 
+scalars_obj = s3.get_object(Bucket="cortex-examples", Key="iris/scalars.json")
+scalars = json.loads(scalars_obj["Body"].read().decode("utf-8"))
+logger.info("downloaded scalars: {}".format(scalars))
+
 
 def pre_inference(sample, metadata):
-    return [
-        sample["sepal_length"],
-        sample["sepal_width"],
-        sample["petal_length"],
-        sample["petal_width"],
-    ]
+    x = np.array(
+        [
+            sample["sepal_length"],
+            sample["sepal_width"],
+            sample["petal_length"],
+            sample["petal_width"],
+        ]
+    )
+    return ((x - scalars["mean"]) / scalars["stddev"]).astype(np.float32)
 
 
 def post_inference(prediction, metadata):
