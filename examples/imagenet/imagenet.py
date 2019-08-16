57b0d418c8fa658d97d4a01929ec6aba18e8a4b1
Ivan Zhang <ivan@ivanzhang.ca>
2019-08-15 21:45:47 -0400

    predict a truck

--- examples/imagenet/imagenet.py
+++ examples/imagenet/imagenet.py
@@ -1,14 +1,27 @@
 import requests
 import numpy as np
+import base64
+from PIL import Image
+from io import BytesIO
+import math
+from cortex.lib.log import get_logger
+
+logger = get_logger()
 
 labels = requests.get(
     "https://storage.googleapis.com/download.tensorflow.org/data/ImageNetLabels.txt"
-)
+).text.split('\n')
 
 
 def pre_inference(sample, metadata):
-    return {"images": [np.zeros((299, 299, 3)).tolist()]}
+    decoded = base64.b64decode(sample["image"])
+    decoded_image = np.asarray(Image.open(BytesIO(decoded)), dtype=np.float32)/255
+    logger.info(decoded_image)
+    return {"images": [decoded_image.tolist()]}
 
 
 def post_inference(prediction, metadata):
-    return {"sentiment": labels[prediction["response"]["labels"][0]]}
+    classes = prediction["response"]["classes"]
+    return {
+        "class": labels[np.argmax(classes)]
+    }
