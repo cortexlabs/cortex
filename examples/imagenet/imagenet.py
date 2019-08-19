87d8508085aa458a3257ec3e819b9c7835dff678
ivan <ivanzhangsolutions@gmail.com>
2019-08-19 18:58:54 -0400

    remove logs

--- examples/imagenet/imagenet.py
+++ examples/imagenet/imagenet.py
@@ -4,9 +4,7 @@ import base64
 from PIL import Image
 from io import BytesIO
 import math
-from cortex.lib.log import get_logger
 
-logger = get_logger()
 
 labels = requests.get(
     "https://storage.googleapis.com/download.tensorflow.org/data/ImageNetLabels.txt"
@@ -16,7 +14,6 @@ labels = requests.get(
 def pre_inference(sample, metadata):
     response = requests.get(sample["link"])
     decoded_image = np.asarray(Image.open(BytesIO(response.content)), dtype=np.float32) / 255
-    logger.info(decoded_image)
     return {"images": [decoded_image.tolist()]}
 
 
