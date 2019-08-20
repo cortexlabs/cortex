5af74dab239391c8299364818fd89807230b25fa
David Eliahu <deliahu@users.noreply.github.com>
2019-08-19 17:28:33 -0700

    .

--- pkg/workloads/cortex/lib/package.py
+++ pkg/workloads/cortex/lib/package.py
@@ -39,7 +39,7 @@ def get_build_order(python_packages):
 
 
 def get_restricted_packages():
-    req_list = ["pyspark==2.4.2", "tensorflow==1.14.0"]
+    req_list = ["tensorflow==1.14.0"]
     req_files = glob.glob("/src/**/requirements.txt", recursive=True)
 
     for req_file in req_files:
