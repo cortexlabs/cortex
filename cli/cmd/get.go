311c5343f490500e0fbb4a95420550bca6fd4410
Ivan Zhang <ivan@ivanzhang.ca>
2019-08-26 15:07:11 -0400

    move url prompt up

--- cli/cmd/get.go
+++ cli/cmd/get.go
@@ -288,8 +288,8 @@ func describeAPI(name string, resourcesRes *schema.GetResourcesResponse, flagVer
 	apiEndpoint := urls.Join(resourcesRes.APIsBaseURL, anyAPIStatus.Path)
 
 	out := "\n" + console.Bold("url:  ") + apiEndpoint + "\n"
-	out += fmt.Sprintf("%s curl -X POST -H \"Content-Type: application/json\" %s -d @samples.json\n", console.Bold("curl:"), apiEndpoint)
 	out += "\n" + console.Bold("debug url:  ") + apiEndpoint + "?debug=true" + "\n"
+	out += fmt.Sprintf("%s curl -X POST -H \"Content-Type: application/json\" %s -d @samples.json\n", console.Bold("curl:"), apiEndpoint)
 	out += fmt.Sprintf(console.Bold("updated at:")+" %s\n\n", libtime.LocalTimestamp(updatedAt))
 
 	statusTable := table.Table{
