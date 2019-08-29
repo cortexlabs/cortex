e0c943aa0e223b3ff6fcf4a0d8db38fedfd92e26
Ivan Zhang <ivan@ivanzhang.ca>
2019-08-29 17:45:01 -0400

    format

--- pkg/workloads/cortex/lib/packages.py
+++ pkg/workloads/cortex/lib/packages.py
@@ -1,13 +1,9 @@
-
 from subprocess import run
 
 from cortex.lib.exceptions import UserException, CortexException
 
+
 def install(project_path):
-    completed_process = run(
-        "pip3 install -r {}/requirements.txt".format(
-            project_path
-        ).split()
-    )
+    completed_process = run("pip3 install -r {}/requirements.txt".format(project_path).split())
     if completed_process.returncode != 0:
         raise UserException("installing packages")
