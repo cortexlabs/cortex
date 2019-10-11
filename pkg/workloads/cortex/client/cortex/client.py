6588fc44604666f4d08613e8f2d09c4711837894
David Eliahu <deliahu@users.noreply.github.com>
2019-10-11 15:38:29 -0700

    Add CORTEX_VERSION comment to client.py

--- pkg/workloads/cortex/client/cortex/client.py
+++ pkg/workloads/cortex/client/cortex/client.py
@@ -44,7 +44,7 @@ class Client(object):
         self.aws_access_key_id = aws_access_key_id
         self.aws_secret_access_key = aws_secret_access_key
         self.headers = {
-            "CortexAPIVersion": "master",
+            "CortexAPIVersion": "master",  # CORTEX_VERSION
             "Authorization": "CortexAWS {}|{}".format(
                 self.aws_access_key_id, self.aws_secret_access_key
             ),
