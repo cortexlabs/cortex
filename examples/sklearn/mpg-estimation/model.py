ba9a9719c3f3f1730ffd246e3412df203c4f4faa
vishal <vishalbollu@users.noreply.github.com>
2019-11-08 14:06:59 +0000

    Move model training code to main function

--- examples/sklearn/mpg-estimation/model.py
+++ examples/sklearn/mpg-estimation/model.py
@@ -4,18 +4,20 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
-df = pd.read_csv(
-    "https://www.uio.no/studier/emner/sv/oekonomi/ECON4150/v16/statacourse/datafiles/auto.csv"
-)
-df = df.replace("?", np.nan)
-df = df.dropna()
-df = df.drop(["name", "origin", "year"], axis=1)  # drop categorical variables for simplicity
-X = df.drop("mpg", axis=1)
-y = df[["mpg"]]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+if if __name__ == "__main__":
+    df = pd.read_csv(
+        "https://www.uio.no/studier/emner/sv/oekonomi/ECON4150/v16/statacourse/datafiles/auto.csv"
+    )
+    df = df.replace("?", np.nan)
+    df = df.dropna()
+    df = df.drop(["name", "origin", "year"], axis=1)  # drop categorical variables for simplicity
+    X = df.drop("mpg", axis=1)
+    y = df[["mpg"]]
 
-model = LinearRegression()
-model.fit(X_train, y_train)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
 
-mlflow.sklearn.save_model(model, "linreg")
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+
+    mlflow.sklearn.save_model(model, "linreg")
