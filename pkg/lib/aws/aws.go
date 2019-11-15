fabfad444536a37d2cd980e58a6b219c192e12a1
David Eliahu <deliahu@users.noreply.github.com>
2019-11-14 18:00:17 -0800

    generate instance metadata

--- pkg/lib/aws/aws.go
+++ pkg/lib/aws/aws.go
@@ -14,7 +14,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-//go:generate echo "hello"
+//go:generate python3 gen_instance_metadata.py
+//go:generate gofmt -s -w instance_metadata.go
 
 package aws
 
