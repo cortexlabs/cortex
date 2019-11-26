5467ca63e7fe8a0dbfc5de0086d2dfbbc88d8bc3
David Eliahu <deliahu@users.noreply.github.com>
2019-11-26 12:41:40 -0800

    Update no cluster error

--- cli/cmd/cluster.go
+++ cli/cmd/cluster.go
@@ -145,6 +145,8 @@ var infoCmd = &cobra.Command{
 		if err != nil {
 			errors.Exit(err)
 		}
+
+		// note: if modifying this string, search the codebase for it and change all occurrences
 		if strings.Contains(out, "there is no cluster") {
 			errors.Exit()
 		}
@@ -274,6 +276,8 @@ func refreshCachedClusterConfig(awsCreds *AWSCredentials) *clusterconfig.Cluster
 	if err != nil {
 		errors.Exit(err)
 	}
+
+	// note: if modifying this string, search the codebase for it and change all occurrences
 	if strings.Contains(out, "there is no cluster") {
 		errors.Exit()
 	}
