54bffacee387dc9c6eb7e8636326c414fe6c697f
vishal <vishalbollu@users.noreply.github.com>
2019-12-16 14:35:09 -0500

    Put labels loading outside class def in image-classifier def

--- examples/tensorflow/image-classifier/predictor.py
+++ examples/tensorflow/image-classifier/predictor.py
@@ -5,12 +5,13 @@ import numpy as np
 from PIL import Image
 from io import BytesIO
 
+labels = requests.get(
+    "https://storage.googleapis.com/download.tensorflow.org/data/ImageNetLabels.txt"
+).text.split("\n")
+
 
 class TensorFlowPredictor:
     def __init__(self, tf_client, config):
-        self._labels = requests.get(
-            "https://storage.googleapis.com/download.tensorflow.org/data/ImageNetLabels.txt"
-        ).text.split("\n")
         self._tf_client = tf_client
 
     def predict(self, payload):
@@ -18,5 +19,5 @@ class TensorFlowPredictor:
         decoded_image = np.asarray(Image.open(BytesIO(image)), dtype=np.float32) / 255
         model_input = {"images": np.expand_dims(decoded_image, axis=0)}
         prediction = self._tf_client.predict(model_input)
-        return self._labels[np.argmax(prediction["classes"])]
+        return labels[np.argmax(prediction["classes"])]
 
