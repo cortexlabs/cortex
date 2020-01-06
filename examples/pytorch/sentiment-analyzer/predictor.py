cb87a46de3d21ee81810e423cb1fd45bad32d8de
Caleb Kaiser <caleb.kaiser.js@gmail.com>
2020-01-05 23:35:16 -0500

    Fix typos

--- examples/pytorch/sentiment-analyzer/predictor.py
+++ examples/pytorch/sentiment-analyzer/predictor.py
@@ -4,12 +4,12 @@ import requests
 class PythonPredictor:
     def __init__(self, config):
         # Download model file
-        req = requests.get('https://cortex-example-projects.s3-us-west-2.amazonaws.com/export.pkl')
+        req = requests.get("https://cortex-example-projects.s3-us-west-2.amazonaws.com/export.pkl")
         with open("export.pkl", "wb") as model:
             model.write(req.content)
 
         # Initialize model
-        self.predictor = load_learner('.')
+        self.predictor = load_learner("."")
 
 
     def predict(self, payload):
