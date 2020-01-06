7aa9516a3ba1004151309ab40271c64aee435c22
Caleb Kaiser <caleb.kaiser.js@gmail.com>
2020-01-05 23:30:18 -0500

    Add fastai example

---
+++ examples/pytorch/sentiment-analyzer/predictor.py
@@ -0,0 +1,17 @@
+from fastai.text import *
+import requests
+
+class PythonPredictor:
+    def __init__(self, config):
+        # Download model file
+        req = requests.get('https://cortex-example-projects.s3-us-west-2.amazonaws.com/export.pkl')
+        with open("export.pkl", "wb") as model:
+            model.write(req.content)
+
+        # Initialize model
+        self.predictor = load_learner('.')
+
+
+    def predict(self, payload):
+        prediction = self.predictor.predict(payload["text"])
+        return prediction[0].obj
