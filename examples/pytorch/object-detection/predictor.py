909043c29e9602d33ad69abca1665732a0c1ff85
David Eliahu <deliahu@users.noreply.github.com>
2020-01-12 10:10:47 -0800

    Format code, extract coco labels
    Changes:
    
    * Format code using [Black](https://pypi.org/project/black/)
    * Extract coco labels to text file (since when Black formatted the array with a new line per item, it took up a lot of space)
    * Update version warning in file comments
    * Remove `print(config)`

--- examples/pytorch/object-detection/predictor.py
+++ examples/pytorch/object-detection/predictor.py
@@ -1,4 +1,4 @@
-# this is an example for cortex release 0.12 and may not deploy correctly on other releases of cortex
+# WARNING: you are on the master branch, please refer to the examples on the branch that matches your `cortex version`
 
 from io import BytesIO
 
@@ -11,29 +11,14 @@ from torchvision import transforms
 
 class PythonPredictor:
     def __init__(self, config):
-        print(config)
         model = models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
         model.eval()
 
-        self.preprocess = transforms.Compose([
-            transforms.ToTensor(),
-        ])
+        self.preprocess = transforms.Compose([transforms.ToTensor()])
 
-        self.coco_labels = [
-            '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
-            'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A', 'stop sign',
-            'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
-            'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack', 'umbrella', 'N/A', 'N/A',
-            'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
-            'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
-            'bottle', 'N/A', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
-            'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
-            'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table',
-            'N/A', 'N/A', 'toilet', 'N/A', 'tv', 'laptop',
-            'mouse', 'remote', 'keyboard', 'cell phone',
-            'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A', 'book',
-            'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush'
-        ]
+        with open("/mnt/project/coco_labels.txt") as f:
+            self.coco_labels = f.read().splitlines()
+
         self.model = model
 
     def predict(self, payload):
@@ -48,14 +33,14 @@ class PythonPredictor:
         with torch.no_grad():
             pred = self.model(img_tensor)
 
-        pred_class = [self.coco_labels[i] for i in list(pred[0]['labels'].numpy())]
-        pred_boxes = [[(i[0], i[1]), (i[2], i[3])] for i in list(pred[0]['boxes'].detach().numpy())]
-        pred_score = list(pred[0]['scores'].detach().numpy())
+        pred_class = [self.coco_labels[i] for i in list(pred[0]["labels"].numpy())]
+        pred_boxes = [[(i[0], i[1]), (i[2], i[3])] for i in list(pred[0]["boxes"].detach().numpy())]
+        pred_score = list(pred[0]["scores"].detach().numpy())
         pred_t = [pred_score.index(x) for x in pred_score if x > threshold]
         if len(pred_t) == 0:
             return [], []
 
         pred_t = pred_t[-1]
-        pred_boxes = pred_boxes[:pred_t + 1]
-        pred_class = pred_class[:pred_t + 1]
+        pred_boxes = pred_boxes[: pred_t + 1]
+        pred_class = pred_class[: pred_t + 1]
         return pred_boxes, pred_class
