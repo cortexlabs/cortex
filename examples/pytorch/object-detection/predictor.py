99e9efe70e43f8657f5e836cd6b1c8c2d4f10439
ArkinDharawat <arkinrd2@illinois.edu>
2020-01-12 19:34:51 +0530

    Added object detection example

---
+++ examples/pytorch/object-detection/predictor.py
@@ -0,0 +1,59 @@
+# this is an example for cortex release 0.12 and may not deploy correctly on other releases of cortex
+
+import requests
+import torch
+from torchvision import models
+from torchvision import transforms
+from PIL import Image
+from io import BytesIO
+
+
+class PythonPredictor:
+    def __init__(self, config):
+        print(config)
+        model = models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+        model.eval()
+
+        self.preprocess = transforms.Compose([
+            transforms.ToTensor(),
+        ])
+
+        self.coco_labels = [
+            '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
+            'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A', 'stop sign',
+            'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
+            'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack', 'umbrella', 'N/A', 'N/A',
+            'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+            'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
+            'bottle', 'N/A', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
+            'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+            'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table',
+            'N/A', 'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
+            'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A', 'book',
+            'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush'
+        ]
+        self.model = model
+
+    def predict(self, payload):
+        threshold = float(payload["threshold"])
+        image = requests.get(payload["url"]).content
+        img_pil = Image.open(BytesIO(image))
+
+        img_tensor = self.preprocess(img_pil)
+
+        img_tensor.unsqueeze_(0)
+
+        with torch.no_grad():
+            pred = self.model(img_tensor)
+
+        pred_class = [self.coco_labels[i] for i in list(pred[0]['labels'].numpy())]
+        pred_boxes = [[(i[0], i[1]), (i[2], i[3])] for i in list(pred[0]['boxes'].detach().numpy())]
+        pred_score = list(pred[0]['scores'].detach().numpy())
+        pred_t = [pred_score.index(x) for x in pred_score if x > threshold]
+        if len(pred_t) == 0:
+            return [], []
+        else:
+            pred_t = pred_t[-1]
+            pred_boxes = pred_boxes[:pred_t + 1]
+            pred_class = pred_class[:pred_t + 1]
+            return pred_boxes, pred_class
