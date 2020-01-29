5b4ab6a97bf615bf6856b8d060920cdba491e17b
David Eliahu <deliahu@users.noreply.github.com>
2020-01-29 12:08:11 -0800

    Cause replica to error when pip install fails
    (cherry picked from commit 394862b0f9ce303f71ce9d18996c13350d47e2c5)

--- pkg/workloads/cortex/serve/run.sh
+++ pkg/workloads/cortex/serve/run.sh
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+set -e
+
 export PYTHONPATH=$PYTHONPATH:$PYTHON_PATH
 
 if [ -f "/mnt/project/requirements.txt" ]; then
