d65f8c9d793e141d24eabe2a347f831ecdc547e9
Robert Lucian Chiriac <robert.lucian.chiriac@gmail.com>
2020-02-02 01:47:58 +0200

    modify the predictor module

--- examples/tensorflow/license-plate-reader/predictor.py
+++ examples/tensorflow/license-plate-reader/predictor.py
@@ -3,13 +3,15 @@ import json
 import base64
 import numpy as np
 import cv2
+import pickle
 from utils.utils import get_yolo_boxes
+from utils.bbox import BoundBox
 
 class TensorFlowPredictor():
     def __init__(self, tensorflow_client, config):
         self.client = tensorflow_client
 
-        with open(config['model_config']) as json_file:
+        with open(config["model_config"]) as json_file:
             data = json.load(json_file)
         for key in data:
             setattr(self, key, data[key])
@@ -21,46 +23,12 @@ class TensorFlowPredictor():
         image = cv2.imdecode(jpg_as_np, flags=cv2.IMREAD_COLOR)
         
         boxes = get_yolo_boxes(self.client, [image], self.net_h, self.net_w,
-        self.anchors, self.obj_thresh, self.nms_thresh, len(self.labels))[0]
-        no_license_plates = len(boxes)
+        self.anchors, self.obj_thresh, self.nms_thresh, len(self.labels))
 
-        return no_license_plates
+        response = {"boxes": []}
+        for box in boxes:
+            response["boxes"].append([
+                box.xmin, box.ymin, box.xmax, box.ymax, box.c, box.classes
+            ])
 
-# import boto3
-# import json
-# import re
-# import os
-# import base64
-# import numpy as np
-# import cv2
-# import keras
-# import tensorflow as tf
-# from os.path import split
-# from utils.utils import get_yolo_boxes
-
-# class PythonPredictor():
-#     def __init__(self, config):
-#         bucket, key = re.match("s3://(.+?)/(.+)", config["model"]).groups()
-#         _, model_name = split(key)
-
-#         s3 = boto3.client("s3")
-#         s3.download_file(bucket, key, model_name)
-#         self.model = keras.models.load_model(model_name)
-
-#         with open(config['model_config']) as json_file:
-#             data = json.load(json_file)
-#         for key in data:
-#             setattr(self, key, data[key])
-
-
-#     def predict(self, payload):
-#         img = payload["img"]
-#         img = base64.b64decode(img)
-#         jpg_as_np = np.frombuffer(img, dtype=np.uint8)
-#         image = cv2.imdecode(jpg_as_np, flags=cv2.IMREAD_COLOR)
-
-#         boxes = get_yolo_boxes(self.model, [image], self.net_h, self.net_w,
-#         self.anchors, self.obj_thresh, self.nms_thresh)[0]
-#         no_license_plates = len(boxes)
-
-#         return no_license_plates
+        return response
