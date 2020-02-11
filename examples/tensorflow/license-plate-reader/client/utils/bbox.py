5ed4eafb7fa7e17e8d415f0a0099fd291987ee37
Robert Lucian Chiriac <robert.lucian.chiriac@gmail.com>
2020-02-11 16:11:39 +0200

    remove unnecessary bits of code

--- examples/tensorflow/license-plate-reader/client/utils/bbox.py
+++ examples/tensorflow/license-plate-reader/client/utils/bbox.py
@@ -66,31 +66,3 @@ def draw_boxes(image, boxes, overlay_text, labels, obj_thresh, quiet=True):
                         thickness=1)
         
     return image
-
-def _interval_overlap(interval_a, interval_b):
-    x1, x2 = interval_a
-    x3, x4 = interval_b
-
-    if x3 < x1:
-        if x4 < x1:
-            return 0
-        else:
-            return min(x2,x4) - x1
-    else:
-        if x2 < x3:
-             return 0
-        else:
-            return min(x2,x4) - x3  
-
-def bbox_iou(box1, box2):
-    intersect_w = _interval_overlap([box1.xmin, box1.xmax], [box2.xmin, box2.xmax])
-    intersect_h = _interval_overlap([box1.ymin, box1.ymax], [box2.ymin, box2.ymax])  
-    
-    intersect = intersect_w * intersect_h
-
-    w1, h1 = box1.xmax-box1.xmin, box1.ymax-box1.ymin
-    w2, h2 = box2.xmax-box2.xmin, box2.ymax-box2.ymin
-    
-    union = w1*h1 + w2*h2 - intersect
-    
-    return float(intersect) / union
