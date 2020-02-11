be56ddf1d1104d5334501f531542e776793edb79
Robert Lucian Chiriac <robert.lucian.chiriac@gmail.com>
2020-02-11 16:14:05 +0200

    remove unnecessary function for the yolov3 predictor

--- examples/tensorflow/license-plate-reader/utils/bbox.py
+++ examples/tensorflow/license-plate-reader/utils/bbox.py
@@ -27,38 +27,6 @@ class BoundBox:
             
         return self.score
 
-def draw_boxes(image, boxes, labels, obj_thresh, quiet=True):
-    for box in boxes:
-        label_str = ''
-        label = -1
-        
-        for i in range(len(labels)):
-            if box.classes[i] > obj_thresh:
-                if label_str != '': label_str += ', '
-                label_str += (labels[i] + ' ' + str(round(box.get_score()*100, 2)) + '%')
-                label = i
-            if not quiet: print(label_str)
-                
-        if label >= 0:
-            text_size = cv2.getTextSize(label_str, cv2.FONT_HERSHEY_SIMPLEX, 1.1e-3 * image.shape[0], 5)
-            width, height = text_size[0][0], text_size[0][1]
-            region = np.array([[box.xmin-3,        box.ymin], 
-                               [box.xmin-3,        box.ymin-height-26], 
-                               [box.xmin+width+13, box.ymin-height-26], 
-                               [box.xmin+width+13, box.ymin]], dtype='int32')  
-
-            cv2.rectangle(img=image, pt1=(box.xmin,box.ymin), pt2=(box.xmax,box.ymax), color=get_color(label), thickness=5)
-            cv2.fillPoly(img=image, pts=[region], color=get_color(label))
-            cv2.putText(img=image, 
-                        text=label_str, 
-                        org=(box.xmin+13, box.ymin - 13), 
-                        fontFace=cv2.FONT_HERSHEY_SIMPLEX, 
-                        fontScale=1e-3 * image.shape[0], 
-                        color=(0,0,0), 
-                        thickness=2)
-        
-    return image
-
 def _interval_overlap(interval_a, interval_b):
     x1, x2 = interval_a
     x3, x4 = interval_b
