a094ec551d3f6558d1c82f5f2d46f3b847553e1b
Robert Lucian Chiriac <robert.lucian.chiriac@gmail.com>
2020-03-07 00:56:10 +0200

    Fix "CUDA_ERROR_OUT_OF_MEMORY: out of memory" error

--- examples/tensorflow/license-plate-reader/predictor_crnn.py
+++ examples/tensorflow/license-plate-reader/predictor_crnn.py
@@ -5,10 +5,19 @@ import numpy as np
 import keras_ocr
 import base64
 import pickle
+import tensorflow as tf
 
 
 class PythonPredictor:
     def __init__(self, config):
+        # limit memory usage on each process
+        gpus = tf.config.experimental.list_physical_devices("GPU")
+        if gpus:
+            try:
+                for gpu in gpus:
+                    tf.config.experimental.set_memory_growth(gpu, True)
+            except RuntimeError as e:
+                print(e)
         # keras-ocr will automatically download pretrained
         # weights for the detector and recognizer.
         self.pipeline = keras_ocr.pipeline.Pipeline()
