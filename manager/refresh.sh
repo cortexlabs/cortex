8b5cdbf696b72a4704c472351ba19c309310eb8c
vishal <vishalbollu@users.noreply.github.com>
2020-03-24 15:16:20 -0400

    Update refresh.sh

--- manager/refresh.sh
+++ manager/refresh.sh
@@ -33,4 +33,4 @@ kubectl -n=default create configmap 'cluster-config' \
     --from-file='cluster.yaml'=tmp_cluster_config.yaml \
     -o yaml --dry-run | kubectl apply -f - >/dev/null
 
-cp tmp_cluster_config.yaml $cached_cluster_config_file
+cat tmp_cluster_config.yaml > $cached_cluster_config_file
