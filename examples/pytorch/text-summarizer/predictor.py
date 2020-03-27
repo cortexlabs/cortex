49da0942cd0651773bc057e48dd0538de815dbb9
David Eliahu <deliahu@users.noreply.github.com>
2020-03-27 16:12:34 -0700

    Replace existing text summarizer example

--- examples/pytorch/text-summarizer/predictor.py
+++ examples/pytorch/text-summarizer/predictor.py
@@ -1,11 +1,16 @@
 # WARNING: you are on the master branch, please refer to the examples on the branch that matches your `cortex version`
 
-from summarizer import Summarizer
+import torch
+from transformers import pipeline
 
 
 class PythonPredictor:
     def __init__(self, config):
-        self.model = Summarizer()
+        device = 0 if torch.cuda.is_available() else -1
+        self.summarizer = pipeline(task="summarization", device=device)
 
     def predict(self, payload):
-        return self.model(payload["text"])
+        summary = self.summarizer(
+            payload["text"], num_beams=4, length_penalty=2.0, max_length=142, no_repeat_ngram_size=3
+        )
+        return summary[0]["summary_text"]
