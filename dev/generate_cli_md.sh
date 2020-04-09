2717ebd26250795c6fee2164b625795a62e6a9b5
David Eliahu <deliahu@users.noreply.github.com>
2020-04-09 11:38:18 -0700

    Create generate_cli_md.sh

---
+++ dev/generate_cli_md.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Copyright 2020 Cortex Labs, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")"/.. >/dev/null && pwd)"
+
+# Clear default environments
+cli_config_backup_path=$HOME/.cortex/cli-bak-$RANDOM.yaml
+mv $HOME/.cortex/cli.yaml $cli_config_backup_path
+
+for command in "deploy" "get" "logs" "refresh" "predict" "delete" "cluster up" "cluster info" "cluster update" "cluster down" "env configure" "env list" "env default" "env delete" "version" "completion"; do
+  echo "## ${command}"
+  echo
+  echo '```text'
+  $ROOT/bin/cortex help ${command}
+  echo '```'
+  echo
+done
+
+# Bring back CLI config
+mv -f $cli_config_backup_path $HOME/.cortex/cli.yaml
