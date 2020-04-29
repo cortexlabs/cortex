12246daaa591e47d5c503625bd1eceea7d7478ed
vishal <vishalbollu@users.noreply.github.com>
2020-04-29 12:32:28 -0400

    Fix mpg-estimator

--- examples/sklearn/mpg-estimator/predictor.py
+++ examples/sklearn/mpg-estimator/predictor.py
@@ -1,12 +1,28 @@
 # WARNING: you are on the master branch, please refer to the examples on the branch that matches your `cortex version`
 
+import boto3
+from botocore import UNSIGNED
+from botocore.client import Config
 import mlflow.sklearn
 import numpy as np
+import re
+import os
 
 
 class PythonPredictor:
     def __init__(self, config):
-        self.model = mlflow.sklearn.load_model(config["model"])
+        model_path = "/tmp/model"
+        os.makedirs(model_path, exist_ok=True)
+
+        # download mlflow model folder from S3 using unsigned (anonymous) aws credentials
+        s3 = boto3.client("s3", config=Config(signature_version=UNSIGNED))
+        bucket, prefix = re.match("s3://(.+?)/(.+)", config["model"]).groups()
+        response = s3.list_objects_v2(Bucket=bucket, Prefix=prefix)
+        for s3_obj in response["Contents"]:
+            obj_key = s3_obj["Key"]
+            s3.download_file(bucket, obj_key, os.path.join(model_path, os.path.basename(obj_key)))
+
+        self.model = mlflow.sklearn.load_model(model_path)
 
     def predict(self, payload):
         model_input = [
