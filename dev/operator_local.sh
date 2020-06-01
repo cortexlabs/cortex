7ff0266eeb9d14e95cee1b7a9818e1056e873339
vishal <vishalbollu@users.noreply.github.com>
2020-06-01 18:45:59 +0000

    Revert "Add operator-local make command"
    This reverts commit 83910d30320babcb57ca5a9b282c41faee5f25cf.

--- dev/operator_local.sh
+++ dev/operator_local.sh
@@ -17,8 +17,6 @@
 
 set -euo pipefail
 
-arg1=${1:-""}
-
 ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")"/.. >/dev/null && pwd)"
 
 kill $(pgrep -f make) >/dev/null 2>&1 || true
@@ -36,40 +34,20 @@ fi
 export CORTEX_OPERATOR_IN_CLUSTER=false
 export CORTEX_CLUSTER_CONFIG_PATH=~/.cortex/cluster-dev.yaml
 
-clear
-mkdir -p ./bin
+clear && echo "starting local operator..."
 
-if [ "$arg1" != "--operator-only" ]; then
-  echo "building cli..."
-  go build -o $ROOT/bin/cortex $ROOT/cli
-fi
-
-echo "building operator..."
-go build -o $ROOT/bin/operator $ROOT/pkg/operator
-
-echo "starting local operator..."
-($ROOT/bin/operator &)
+mkdir -p ./bin && go build -o $ROOT/bin/cortex $ROOT/cli && go build -o $ROOT/bin/operator $ROOT/pkg/operator && ($ROOT/bin/operator &)
 
 trap ctrl_c INT
 function ctrl_c() {
   kill $(pgrep -f /bin/operator) >/dev/null 2>&1
-  exit 1
 }
 
-if [ "$arg1" = "--operator-only" ]; then
-  watchmedo shell-command \
-    --command='kill $(pgrep -f /bin/operator);'" clear && echo 'rebuilding operator...' && go build -o $ROOT/bin/operator $ROOT/pkg/operator && echo 'starting local operator...' && $ROOT/bin/operator &" \
-    --patterns '*.go;*.yaml' \
-    --recursive \
-    --drop \
-    $ROOT/pkg $ROOT/dev/config
-else
-  watchmedo shell-command \
-    --command='kill $(pgrep -f /bin/operator);'" clear && echo 'rebuilding cli...' && go build -o $ROOT/bin/cortex $ROOT/cli && echo 'rebuilding operator...' && go build -o $ROOT/bin/operator $ROOT/pkg/operator && echo 'starting local operator...' && $ROOT/bin/operator &" \
-    --patterns '*.go;*.yaml' \
-    --recursive \
-    --drop \
-    $ROOT/cli $ROOT/pkg $ROOT/dev/config
-fi
+watchmedo shell-command \
+  --command='kill $(pgrep -f /bin/operator);'" clear && echo 'restarting local operator...' && go build -o $ROOT/bin/cortex $ROOT/cli && go build -o $ROOT/bin/operator $ROOT/pkg/operator && $ROOT/bin/operator &" \
+  --patterns '*.go;*.yaml' \
+  --recursive \
+  --drop \
+  $ROOT/pkg $ROOT/cli $ROOT/dev/config
 
 # go run -race $ROOT/pkg/operator/main.go  # Check for race conditions. Doesn't seem to catch them all?
