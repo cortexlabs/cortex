71e3c43c676bd16fd38375ce5409029f68cb1075
David Eliahu <deliahu@users.noreply.github.com>
2020-06-11 08:14:29 -0700

    Allow unbound variables in install.sh

--- manager/install.sh
+++ manager/install.sh
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -euo pipefail
+set -eo pipefail
 
 export CORTEX_VERSION=master
 EKSCTL_TIMEOUT=45m
