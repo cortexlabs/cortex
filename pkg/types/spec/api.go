8c70553c346018934466470f10e0de53e5c6b7ae
David Eliahu <deliahu@users.noreply.github.com>
2020-06-22 20:34:23 -0700

    Prevent endpoint updates from triggering rolling replica updates

--- pkg/types/spec/api.go
+++ pkg/types/spec/api.go
@@ -50,9 +50,6 @@ type LocalModelCache struct {
 func GetAPISpec(apiConfig *userconfig.API, projectID string, deploymentID string) *API {
 	var buf bytes.Buffer
 	buf.WriteString(apiConfig.Name)
-	if apiConfig.Networking.Endpoint != nil {
-		buf.WriteString(*apiConfig.Networking.Endpoint)
-	}
 	if apiConfig.Networking.LocalPort != nil {
 		buf.WriteString(s.Obj(*apiConfig.Networking.LocalPort))
 	}
