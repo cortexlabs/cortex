57444d2f710e4be9c6a0e8c64883cdcad2acbeb3
David Eliahu <deliahu@users.noreply.github.com>
2020-08-06 17:13:00 -0700

    Update example

--- examples/pytorch/text-generator/predictor.py
+++ examples/pytorch/text-generator/predictor.py
@@ -6,11 +6,16 @@ from transformers import GPT2Tokenizer, GPT2LMHeadModel
 
 class PythonPredictor:
     def __init__(self, config):
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        print(f"using device: {self.device}")
         self.tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
-        self.model = GPT2LMHeadModel.from_pretrained("gpt2")
+        self.model = GPT2LMHeadModel.from_pretrained("gpt2").to(self.device)
 
-    def predict(self, payload, query_params, headers):
-        tokens = self.tokenizer.encode(payload["text"])
-        prediction = self.model(torch.tensor([tokens]))
-        token = torch.argmax(prediction[0][0, -1, :]).item()
-        return self.tokenizer.decode(tokens + [token])
+    def predict(self, payload, query_params):
+        text = payload["text"]
+        new_words_to_generate = int(query_params.get("words", 20))
+        total_words = len(text.split(" ")) + new_words_to_generate
+
+        tokens = self.tokenizer.encode(text, return_tensors="pt").to(self.device)
+        prediction = self.model.generate(tokens, max_length=total_words, do_sample=True)
+        return self.tokenizer.decode(prediction[0])
