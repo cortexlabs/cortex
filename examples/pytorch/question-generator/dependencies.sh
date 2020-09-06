794bb32324b0e902a96f91fa1ce14fd81a1b90a3
Robert Lucian Chiriac <robert.lucian.chiriac@gmail.com>
2020-09-06 04:01:51 +0300

    Make lint

--- examples/pytorch/question-generator/dependencies.sh
+++ examples/pytorch/question-generator/dependencies.sh
@@ -1,1 +1,3 @@
+# WARNING: you are on the master branch; please refer to examples on the branch corresponding to your `cortex version` (e.g. for version 0.19.*, run `git checkout -b 0.19` or switch to the `0.19` branch on GitHub)
+
 pip uninstall torchvision -y
