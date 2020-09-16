2da3136b5cfc5175f062a685fa8ca4c2070f225d
David Eliahu <deliahu@users.noreply.github.com>
2020-09-16 12:33:35 -0700

    Update cluster up error message

--- cli/cmd/cluster.go
+++ cli/cmd/cluster.go
@@ -176,7 +176,7 @@ var _upCmd = &cobra.Command{
 			if err != nil {
 				helpStr := "\ndebugging tips (may or may not apply to this error):"
 				helpStr += fmt.Sprintf("\n* if your cluster started spinning up but was unable to provision instances, additional error information may be found in the activity history of your cluster's autoscaling groups (select each autoscaling group and click the \"Activity\" or \"Activity History\" tab): https://console.aws.amazon.com/ec2/autoscaling/home?region=%s#AutoScalingGroups:", *clusterConfig.Region)
-				helpStr += fmt.Sprintf("\n* if your cluster started spinning up, please ensure that your CloudFormation stacks for this cluster have been fully deleted before trying to spin up this cluster again; you can delete your CloudFormation stacks from the AWS console: %s", clusterstate.CloudFormationURL(clusterConfig.ClusterName, *clusterConfig.Region))
+				helpStr += "\n* if your cluster started spinning up, please run `cortex cluster down` to delete the cluster before trying to create this cluster again"
 				fmt.Println(helpStr)
 				exit.Error(ErrorClusterUp(out + helpStr))
 			}
@@ -191,14 +191,14 @@ var _upCmd = &cobra.Command{
 			if err != nil {
 				helpStr := "\ndebugging tips (may or may not apply to this error):"
 				helpStr += fmt.Sprintf("\n* if your cluster was unable to provision instances, additional error information may be found in the activity history of your cluster's autoscaling groups (select each autoscaling group and click the \"Activity\" or \"Activity History\" tab): https://console.aws.amazon.com/ec2/autoscaling/home?region=%s#AutoScalingGroups:", *clusterConfig.Region)
-				helpStr += fmt.Sprintf("\n* please ensure that your CloudFormation stacks for this cluster have been fully deleted before trying to spin up this cluster again; you can delete your CloudFormation stacks from the AWS console: %s", clusterstate.CloudFormationURL(clusterConfig.ClusterName, *clusterConfig.Region))
+				helpStr += "\n* please run `cortex cluster down` to delete the cluster before trying to create this cluster again"
 				fmt.Println(helpStr)
 				exit.Error(ErrorClusterUp(out + helpStr))
 			}
 
 			// no autoscaling groups were created
 			if len(asgs) == 0 {
-				helpStr := fmt.Sprintf("\nplease ensure that your CloudFormation stacks for this cluster have been fully deleted before trying to spin up this cluster again; you can delete your CloudFormation stacks from the AWS console: %s", clusterstate.CloudFormationURL(clusterConfig.ClusterName, *clusterConfig.Region))
+				helpStr := "\nplease run `cortex cluster down` to delete the cluster before trying to create this cluster again"
 				fmt.Println(helpStr)
 				exit.Error(ErrorClusterUp(out + helpStr))
 			}
@@ -208,7 +208,7 @@ var _upCmd = &cobra.Command{
 				if err != nil {
 					helpStr := "\ndebugging tips (may or may not apply to this error):"
 					helpStr += fmt.Sprintf("\n* if your cluster was unable to provision instances, additional error information may be found in the activity history of your cluster's autoscaling groups (select each autoscaling group and click the \"Activity\" or \"Activity History\" tab): https://console.aws.amazon.com/ec2/autoscaling/home?region=%s#AutoScalingGroups:", *clusterConfig.Region)
-					helpStr += fmt.Sprintf("\n* please ensure that your CloudFormation stacks for this cluster have been fully deleted before trying to spin up this cluster again; you can delete your CloudFormation stacks from the AWS console: %s", clusterstate.CloudFormationURL(clusterConfig.ClusterName, *clusterConfig.Region))
+					helpStr += "\n* please run `cortex cluster down` to delete the cluster before trying to create this cluster again"
 					fmt.Println(helpStr)
 					exit.Error(ErrorClusterUp(out + helpStr))
 				}
@@ -226,14 +226,14 @@ var _upCmd = &cobra.Command{
 					helpStr := "\nyour cluster was unable to provision EC2 instances; here is one of the encountered errors:"
 					helpStr += fmt.Sprintf("\n\n> status: %s\n> description: %s", status, description)
 					helpStr += fmt.Sprintf("\n\nadditional error information might be found in the activity history of your cluster's autoscaling groups (select each autoscaling group and click the \"Activity\" or \"Activity History\" tab): https://console.aws.amazon.com/ec2/autoscaling/home?region=%s#AutoScalingGroups:", *clusterConfig.Region)
-					helpStr += fmt.Sprintf("\n\nplease ensure that your CloudFormation stacks for this cluster have been fully deleted before trying to spin up this cluster again; you can delete your CloudFormation stacks from the AWS console: %s", clusterstate.CloudFormationURL(clusterConfig.ClusterName, *clusterConfig.Region))
+					helpStr += "\n\nplease run `cortex cluster down` to delete the cluster before trying to create this cluster again"
 					fmt.Println(helpStr)
 					exit.Error(ErrorClusterUp(out + helpStr))
 				}
 			}
 
 			// No failed asg activities
-			helpStr := fmt.Sprintf("\nplease ensure that your CloudFormation stacks for this cluster have been fully deleted before trying to spin up this cluster again; you can delete your CloudFormation stacks from the AWS console: %s", clusterstate.CloudFormationURL(clusterConfig.ClusterName, *clusterConfig.Region))
+			helpStr := "\nplease run `cortex cluster down` to delete the cluster before trying to create this cluster again"
 			fmt.Println(helpStr)
 			exit.Error(ErrorClusterUp(out + helpStr))
 		}
