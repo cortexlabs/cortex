a380f953f406a4f0cabb6d40630a467e5476afc6
Vishal Bollu <vishal.bollu@gmail.com>
2020-09-23 14:34:56 -0400

    Update api.go

--- pkg/operator/resources/realtimeapi/api.go
+++ pkg/operator/resources/realtimeapi/api.go
@@ -150,7 +150,7 @@ func RefreshAPI(apiName string, force bool) (string, error) {
 		return "", errors.Wrap(err, "upload api spec")
 	}
 
-	// Use api spec indexed by PredictorID for replicas to prevent rolling updates when SpecID changes without PredictorID changing
+	// Reuploading api spec to the same PredictorID but this time with a different DeploymentID due to refresh
 	if err := config.AWS.UploadJSONToS3(api, config.Cluster.Bucket, api.PredictorKey); err != nil {
 		return "", errors.Wrap(err, "upload predictor spec")
 	}
