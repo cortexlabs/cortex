9e5b6517e7c57c317d055f7e0843286bae45197c
vishal <vishalbollu@users.noreply.github.com>
2020-09-29 13:44:45 -0400

    Add version warning to throughput test

--- examples/utils/throughput_test.py
+++ examples/utils/throughput_test.py
@@ -1,3 +1,5 @@
+# WARNING: you are on the master branch; please refer to examples on the branch corresponding to your `cortex version` (e.g. for version 0.19.*, run `git checkout -b 0.19` or switch to the `0.19` branch on GitHub)
+
 import os
 import sys
 import click
