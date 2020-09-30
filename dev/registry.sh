d131bcdef67285e2d20c930d62c3238bdfda43cc
Robert Lucian Chiriac <robert.lucian.chiriac@gmail.com>
2020-09-30 04:01:42 +0300

    Fix make registry-all failing

--- dev/registry.sh
+++ dev/registry.sh
@@ -181,6 +181,14 @@ function cleanup_ecr() {
   done
 }
 
+# export functions for parallel command
+export -f build_and_push
+export -f push
+export -f build
+export -f blue_echo
+export -f green_echo
+export -f ecr_login
+
 # images to build
 all_images="
 tensorflow-serving-cpu
@@ -244,8 +252,8 @@ elif [ "$cmd" = "update" ]; then
 
   images_to_build="${images_to_build}${base_images}"
 
-  if [ command -v parallel &> /dev/null ]; then
-    parallel --colsep=" " build_and_push $ROOT/images/{1} {1} latest ::: $final
+  if command -v parallel &> /dev/null ; then
+    ROOT=$ROOT REGISTRY_URL=$REGISTRY_URL SHELL=$(type -p /bin/bash) parallel --colsep=" " build_and_push "$ROOT/images/{1} {1} latest" ::: $all_images
   else
     for image in ${images_to_build}; do
       build_and_push $ROOT/images/$image $image latest
