8cf39debe3e3e31e2ee3ef8dd109a89a0d76fda2
vishal <vishalbollu@users.noreply.github.com>
2020-10-16 12:27:15 -0400

    Remove cortex.zip after upload

--- build/cli.sh
+++ build/cli.sh
@@ -40,6 +40,7 @@ function build_and_upload() {
     zip cortex.zip cortex
     echo "Uploading zipped Cortex CLI to s3://$CLI_BUCKET_NAME/$CORTEX_VERSION/cli/$os/cortex.zip"
     aws s3 cp cortex.zip s3://$CLI_BUCKET_NAME/$CORTEX_VERSION/cli/$os/cortex.zip --only-show-errors
+    rm cortex.zip
   fi
   echo "Done ✓"
   rm cortex
