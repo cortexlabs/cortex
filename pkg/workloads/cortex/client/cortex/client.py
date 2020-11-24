009863c5db8c060f26a8f6936dda15ba20afb34a
Vishal Bollu <vishal.bollu@gmail.com>
2020-11-23 22:42:54 -0500

    Accept class definition from Python Client directly (#1587)

--- pkg/workloads/cortex/client/cortex/client.py
+++ pkg/workloads/cortex/client/cortex/client.py
@@ -18,9 +18,15 @@ import time
 import sys
 import subprocess
 import threading
+import yaml
+import uuid
+import dill
+import inspect
+from pathlib import Path
 
 from typing import List, Dict, Optional, Tuple, Callable, Union
 from cortex.binary import run_cli, get_cli_path
+from cortex import util
 
 
 class Client:
@@ -33,7 +39,107 @@ class Client:
         """
         self.env = env
 
+    # CORTEX_VERSION_MINOR x5
     def deploy(
+        self,
+        api_spec: dict,
+        predictor=None,
+        pip_dependencies=[],
+        conda_dependencies=[],
+        project_dir: Optional[str] = None,
+        force: bool = False,
+        wait: bool = False,
+    ) -> list:
+        """
+        Deploy an API.
+
+        Args:
+            api_spec: A dictionary defining a single Cortex API. Schema can be found here:
+                → Realtime API: https://docs.cortex.dev/v/master/deployments/realtime-api/api-configuration
+                → Batch API: https://docs.cortex.dev/v/master/deployments/batch-api/api-configuration
+                → Traffic Splitter: https://docs.cortex.dev/v/master/deployments/realtime-api/traffic-splitter
+            predictor: A Cortex Predictor class implementation. Not required when deploying a traffic splitter.
+                → Realtime API: https://docs.cortex.dev/v/master/deployments/realtime-api/predictors
+                → Batch API: https://docs.cortex.dev/v/master/deployments/batch-api/predictors
+            pip_dependencies: A list of PyPI dependencies that will be installed before the predictor class implementation is invoked.
+            conda_dependencies: A list of Conda dependencies that will be installed before the predictor class implementation is invoked.
+            project_dir: Path to a python project.
+            force: Override any in-progress api updates.
+            wait: Streams logs until the APIs are ready.
+
+        Returns:
+            Deployment status, API specification, and endpoint for each API.
+        """
+
+        if project_dir is not None and predictor is not None:
+            raise ValueError(
+                "`predictor` and `project_dir` parameters cannot be specified at the same time, please choose one"
+            )
+
+        if project_dir is not None:
+            cortex_yaml_path = os.path.join(project_dir, f".cortex-{uuid.uuid4()}.yaml")
+
+            with util.open_temporarily(cortex_yaml_path, "w") as f:
+                yaml.dump([api_spec], f)  # write a list
+                return self._deploy(cortex_yaml_path, force, wait)
+
+        project_dir = Path.home() / ".cortex" / "deployments" / str(uuid.uuid4())
+        with util.open_tempdir(str(project_dir)):
+            cortex_yaml_path = os.path.join(project_dir, "cortex.yaml")
+
+            if predictor is None:
+                # for deploying a traffic splitter
+                with open(cortex_yaml_path, "w") as f:
+                    yaml.dump([api_spec], f)  # write a list
+                    return self._deploy(cortex_yaml_path, force=force, wait=wait)
+
+            # Change if PYTHONVERSION changes
+            expected_version = "3.6"
+            actual_version = f"{sys.version_info.major}.{sys.version_info.minor}"
+            if actual_version < expected_version:
+                raise Exception("cortex is only supported for python versions >= 3.6")  # unexpected
+            if actual_version > expected_version:
+                is_python_set = any(
+                    conda_dep.startswith("python=") or "::python=" in conda_dep
+                    for conda_dep in conda_dependencies
+                )
+
+                if not is_python_set:
+                    conda_dependencies = [
+                        f"conda-forge::python={sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}"
+                    ] + conda_dependencies
+
+            if len(pip_dependencies) > 0:
+                with open(project_dir / "requirements.txt", "w") as requirements_file:
+                    requirements_file.write("\n".join(pip_dependencies))
+
+            if len(conda_dependencies) > 0:
+                with open(project_dir / "conda-packages.txt", "w") as conda_file:
+                    conda_file.write("\n".join(conda_dependencies))
+
+            if not inspect.isclass(predictor):
+                raise ValueError("predictor parameter must be a class definition")
+
+            with open(project_dir / "predictor.pickle", "wb") as pickle_file:
+                dill.dump(predictor, pickle_file)
+                if api_spec.get("predictor") is None:
+                    api_spec["predictor"] = {}
+
+                if predictor.__name__ == "PythonPredictor":
+                    predictor_type = "python"
+                if predictor.__name__ == "TensorFlowPredictor":
+                    predictor_type = "tensorflow"
+                if predictor.__name__ == "ONNXPredictor":
+                    predictor_type = "onnx"
+
+                api_spec["predictor"]["path"] = "predictor.pickle"
+                api_spec["predictor"]["type"] = predictor_type
+
+            with open(cortex_yaml_path, "w") as f:
+                yaml.dump([api_spec], f)  # write a list
+                return self._deploy(cortex_yaml_path, force=force, wait=wait)
+
+    def _deploy(
         self,
         config_file: str,
         force: bool = False,
