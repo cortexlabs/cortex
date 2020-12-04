e4298f861105c41ee5215cba8a3b6b9882c25538
David Eliahu <deliahu@users.noreply.github.com>
2020-12-03 18:36:25 -0800

    Update install.sh

--- manager/install.sh
+++ manager/install.sh
@@ -118,7 +118,7 @@ function cluster_up_gcp() {
   kubectl apply -f /workspace/apis.yaml >/dev/null
   echo "✓"
 
-  if [ -z "$CORTEX_ACCELERATOR_TYPE" ]; then
+  if [ -n "$CORTEX_ACCELERATOR_TYPE" ]; then
     echo -n "￮ configuring gpu support "
     cat manifests/nvidia_gcp.yaml | kubectl apply -f - >/dev/null
     echo "✓"
