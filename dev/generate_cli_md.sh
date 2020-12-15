e6f4697c54232e1214f35535a70863b654ca0db5
David Eliahu <deliahu@users.noreply.github.com>
2020-12-15 04:41:24 +0000

    Update docs/workloads/cli.md
    (cherry picked from commit 8da8347bbd133c64c9b7ba3222df9d1061ba591d)

--- dev/generate_cli_md.sh
+++ dev/generate_cli_md.sh
@@ -29,6 +29,8 @@ cli_config_backup_path=$HOME/.cortex/cli-bak-$RANDOM.yaml
 mv $HOME/.cortex/cli.yaml $cli_config_backup_path
 
 echo "# CLI reference" >> $out_file
+echo "" >> $out_file
+echo '_WARNING: you are on the master branch, please refer to the docs on the branch that matches your `cortex version`_' >> $out_file
 
 commands=(
   "deploy"
