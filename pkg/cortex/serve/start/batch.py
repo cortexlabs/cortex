7f70e9cc9eb19bf1a8f52739db61f78a36f494d2
Vishal Bollu <vishal.bollu@gmail.com>
2020-12-18 05:11:46 -0500

    Initial implementation for task runner in python (#1706)

--- pkg/cortex/serve/start/batch.py
+++ pkg/cortex/serve/start/batch.py
@@ -48,9 +48,6 @@ local_cache = {
     "job_spec": None,
     "provider": None,
     "predictor_impl": None,
-    "predict_route": None,
-    "client": None,
-    "class_set": set(),
     "sqs_client": None,
 }
 
