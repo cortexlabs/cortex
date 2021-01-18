62a0dc78990b4ab6ea3a421e65154cbb64b0ff82
Robert Lucian Chiriac <robert.lucian.chiriac@gmail.com>
2021-01-18 23:08:11 +0200

    Fix job endpoints for task/batch APIs

--- pkg/operator/endpoints/get_batch_job.go
+++ pkg/operator/endpoints/get_batch_job.go
@@ -18,8 +18,8 @@ package endpoints
 
 import (
 	"net/http"
+	"net/url"
 
-	"github.com/cortexlabs/cortex/pkg/lib/urls"
 	"github.com/cortexlabs/cortex/pkg/operator/operator"
 	"github.com/cortexlabs/cortex/pkg/operator/resources"
 	"github.com/cortexlabs/cortex/pkg/operator/resources/job/batchapi"
@@ -72,10 +72,18 @@ func GetBatchJob(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	parsedURL, err := url.Parse(endpoint)
+	if err != nil {
+		respondError(w, r, err)
+	}
+	q := parsedURL.Query()
+	q.Add("jobID", jobKey.ID)
+	parsedURL.RawQuery = q.Encode()
+
 	response := schema.BatchJobResponse{
 		JobStatus: *jobStatus,
 		APISpec:   *apiSpec,
-		Endpoint:  urls.Join(endpoint, jobKey.ID),
+		Endpoint:  parsedURL.String(),
 	}
 
 	respond(w, response)
