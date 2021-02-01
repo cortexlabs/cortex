3d304781d934c06593b579a8607a9a23d5bc8818
Robert Lucian Chiriac <robert.lucian.chiriac@gmail.com>
2021-02-01 23:57:19 +0200

    Update k8s from 1.17 to 1.18 (#1842)

--- cli/cmd/cluster_gcp.go
+++ cli/cmd/cluster_gcp.go
@@ -454,7 +454,7 @@ func createGKECluster(clusterConfig *clusterconfig.GCPConfig, gcpClient *gcp.Cli
 
 	gkeClusterConfig := containerpb.Cluster{
 		Name:                  clusterConfig.ClusterName,
-		InitialClusterVersion: "1.17",
+		InitialClusterVersion: "1.18",
 		LoggingService:        "none",
 		NodePools: []*containerpb.NodePool{
 			{
