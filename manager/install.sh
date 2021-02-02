c89e50719b9e758e94ba75800410479a9e2b0f2d
Miguel Varela Ramos <miguel@cortexlabs.com>
2021-02-02 16:26:23 +0100

    Set --install-nvidia-plugin=false in eksctl

--- manager/install.sh
+++ manager/install.sh
@@ -207,7 +207,7 @@ function create_eks() {
 
   echo -e "￮ spinning up the cluster (this will take about 25 minutes) ...\n"
   python generate_eks.py $CORTEX_CLUSTER_CONFIG_FILE > /workspace/eks.yaml
-  eksctl create cluster --timeout=$EKSCTL_TIMEOUT --install-neuron-plugin=false -f /workspace/eks.yaml
+  eksctl create cluster --timeout=$EKSCTL_TIMEOUT --install-neuron-plugin=false --install-nvidia-plugin=false -f /workspace/eks.yaml
   echo
 
   suspend_az_rebalance
