44a77b8b8662c6452f0e036dd91a7cda90d94f86
Miguel Varela Ramos <miguel@cortexlabs.com>
2021-02-12 12:31:54 +0100

    Fix linting errors

--- test/e2e/tests/conftest.py
+++ test/e2e/tests/conftest.py
@@ -78,7 +78,7 @@ def pytest_configure(config):
             ),
             "batch_deploy_timeout": int(os.environ.get("CORTEX_TEST_BATCH_DEPLOY_TIMEOUT", 30)),
             "batch_job_timeout": int(os.environ.get("CORTEX_TEST_BATCH_JOB_TIMEOUT", 200)),
-            "skip_gpus": config.getoption("--skip-gpus")
+            "skip_gpus": config.getoption("--skip-gpus"),
         },
     }
 
