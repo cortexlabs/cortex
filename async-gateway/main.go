8ba77045e1438331008407172450221480d85ce2
Miguel Varela Ramos <miguel@cortexlabs.com>
2021-03-05 18:45:05 +0100

    Update health route

--- async-gateway/main.go
+++ async-gateway/main.go
@@ -123,8 +123,7 @@ func main() {
 	router.HandleFunc(
 		"/healthz",
 		func(w http.ResponseWriter, r *http.Request) {
-			w.WriteHeader(http.StatusOK)
-			_, _ = w.Write([]byte("ok"))
+			respondPlainText(w, http.StatusOK, "ok")
 		},
 	)
 
