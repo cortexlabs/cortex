d3aa8ae3b93c10b013d16a16c2e4513a9b4fc422
Miguel Varela Ramos <miguel@cortexlabs.com>
2021-03-09 15:24:28 +0100

    Added python code for AsyncAPI workloads

---
+++ pkg/cortex/serve/cortex_internal/lib/api/async.py
@@ -0,0 +1,178 @@
+# Copyright 2021 Cortex Labs, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import imp
+import inspect
+import json
+import os
+from copy import deepcopy
+from http import HTTPStatus
+from typing import Any, Dict, Union
+
+import datadog
+import dill
+
+from cortex_internal.lib.api.validations import validate_class_impl
+from cortex_internal.lib.exceptions import CortexException, UserException, UserRuntimeException
+from cortex_internal.lib.metrics import MetricsClient
+from cortex_internal.lib.storage import S3
+
+ASYNC_PYTHON_PREDICTOR_VALIDATION = {
+    "required": [
+        {
+            "name": "__init__",
+            "required_args": ["self", "config"],
+            "optional_args": ["metrics_client"],
+        },
+        {
+            "name": "predict",
+            "required_args": ["self"],
+            "optional_args": ["payload", "request_id"],
+        },
+    ],
+    "optional": [
+        {
+            "name": "post_predict",
+            "required_args": ["self"],
+            "optional_args": ["response", "payload", "request_id"],
+        },
+    ],
+}
+
+
+class AsyncAPI:
+    def __init__(
+        self,
+        api_spec: Dict[str, Any],
+        storage: S3,
+        storage_path: str,
+        statsd_host: str,
+        statsd_port: int,
+    ):
+        self.api_spec = api_spec
+        self.storage = storage
+        self.storage_path = storage_path
+        self.path = api_spec["predictor"]["path"]
+        self.config = api_spec["predictor"].get("config", {})
+
+        datadog.initialize(statsd_host=statsd_host, statsd_port=statsd_port)
+        self.__statsd = datadog.statsd
+
+    @property
+    def statsd(self):
+        return self.__statsd
+
+    def update_status(self, request_id: str, status: str):
+        self.storage.put_str(status, f"{self.storage_path}/{request_id}/status")
+
+    def upload_result(self, request_id: str, result: Dict[str, Any]):
+        try:
+            result_json = json.dumps(result)
+        except Exception:
+            raise UserRuntimeException("user response is not json serializable")
+
+        self.storage.put_str(result_json, f"{self.storage_path}/{request_id}/result.json")
+
+    def get_payload(self, request_id: str) -> Union[Dict, str, bytes]:
+        key = f"{self.storage_path}/{request_id}/payload"
+        obj = self.storage.get_object(key)
+        status_code = obj["ResponseMetadata"]["HTTPStatusCode"]
+        if status_code != HTTPStatus.OK:
+            raise CortexException(
+                f"failed to retrieve async payload (request_id: {request_id}, status_code: {status_code})"
+            )
+
+        content_type = obj["ResponseMetadata"]["HTTPHeaders"]["content-type"]
+        payload_bytes = obj["Body"].read()
+
+        # TODO: discuss
+        # decode payload
+        if content_type == "application/json":
+            return json.loads(payload_bytes)
+        elif content_type == "text/plain":
+            return payload_bytes.decode("utf-8")
+        else:
+            return payload_bytes
+
+    def initialize_impl(self, project_dir: str, metrics_client: MetricsClient):
+        predictor_impl = self._get_impl(project_dir)
+        constructor_args = inspect.getfullargspec(predictor_impl.__init__).args
+        config = deepcopy(self.config)
+
+        args = {}
+        if "config" in constructor_args:
+            args["config"] = config
+        if "metrics_client" in constructor_args:
+            args["metrics_client"] = metrics_client
+
+        try:
+            predictor = predictor_impl(**args)
+        except Exception as e:
+            raise UserRuntimeException(self.path, "__init__", str(e)) from e
+
+        return predictor
+
+    def _get_impl(self, project_dir: str):
+        try:
+            impl = self._read_impl(
+                "cortex_async_predictor", os.path.join(project_dir, self.path), "PythonPredictor"
+            )
+        except CortexException as e:
+            e.wrap("error in " + self.path)
+            raise
+
+        try:
+            self._validate_impl(impl)
+        except CortexException as e:
+            e.wrap("error in " + self.path)
+            raise
+
+        return impl
+
+    @staticmethod
+    def _read_impl(module_name: str, impl_path: str, target_class_name):
+        if impl_path.endswith(".pickle"):
+            try:
+                with open(impl_path, "rb") as pickle_file:
+                    return dill.load(pickle_file)
+            except Exception as e:
+                raise UserException("unable to load pickle", str(e)) from e
+
+        try:
+            impl = imp.load_source(module_name, impl_path)
+        except Exception as e:
+            raise UserException(str(e)) from e
+
+        classes = inspect.getmembers(impl, inspect.isclass)
+
+        if len(classes) > 0:
+            predictor_class = None
+            for class_df in classes:
+                if class_df[0] == target_class_name:
+                    if predictor_class is not None:
+                        raise UserException(
+                            f"multiple definitions for {target_class_name} class found; please check "
+                            f"your imports and class definitions and ensure that there is only one "
+                            f"predictor class definition"
+                        )
+                    predictor_class = class_df[1]
+            if predictor_class is None:
+                raise UserException(f"{target_class_name} class is not defined")
+            return predictor_class
+        else:
+            raise UserException("no callable class or function were provided")
+
+    @staticmethod
+    def _validate_impl(impl):
+        return validate_class_impl(impl, ASYNC_PYTHON_PREDICTOR_VALIDATION)
