a8ec97a367350abcdea6dcfe64a9f99dba72a537
Robert Lucian Chiriac <robert.lucian.chiriac@gmail.com>
2021-03-16 04:46:54 +0200

    Fix indexes in install.sh

--- manager/install.sh
+++ manager/install.sh
@@ -358,9 +358,9 @@ function resize_nodegroup() {
       fi
     done
 
-    desired=$(cat nodegroups.json | jq -r .[$idx].DesiredCapacity)
-    existing_min=$(cat nodegroups.json | jq -r .[$idx].MinSize)
-    existing_max=$(cat nodegroups.json | jq -r .[$idx].MaxSize)
+    desired=$(cat nodegroups.json | jq -r .[$eks_idx].DesiredCapacity)
+    existing_min=$(cat nodegroups.json | jq -r .[$eks_idx].MinSize)
+    existing_max=$(cat nodegroups.json | jq -r .[$eks_idx].MaxSize)
     updating_min=$(cat /in/cluster_${CORTEX_CLUSTER_NAME}_${CORTEX_REGION}.yaml | yq -r .node_groups[$idx].min_instances)
     updating_max=$(cat /in/cluster_${CORTEX_CLUSTER_NAME}_${CORTEX_REGION}.yaml | yq -r .node_groups[$idx].max_instances)
     if [ $updating_min = "null" ]; then
