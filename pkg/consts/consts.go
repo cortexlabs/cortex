3f056316540cd804aebeab101a30ea3dc25b61b7
Vishal Bollu <vishalbollu@users.noreply.github.com>
2021-03-16 19:52:15 +0000

    Update version to 0.31.0

--- pkg/consts/consts.go
+++ pkg/consts/consts.go
@@ -24,8 +24,8 @@ import (
 )
 
 var (
-	CortexVersion      = "master" // CORTEX_VERSION
-	CortexVersionMinor = "master" // CORTEX_VERSION_MINOR
+	CortexVersion      = "0.31.0" // CORTEX_VERSION
+	CortexVersionMinor = "0.31"   // CORTEX_VERSION_MINOR
 
 	SingleModelName = "_cortex_default"
 
