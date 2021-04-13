ed2f607f89b8f66b5716870b7be0466410ac2c25
David Eliahu <deliahu@users.noreply.github.com>
2021-04-12 19:48:41 -0700

    Update push-image.sh

--- build/push-image.sh
+++ build/push-image.sh
@@ -28,8 +28,8 @@ if [ "$image" == "python-predictor-gpu" ]; then
   cuda=("10.0" "10.1" "10.1" "10.2" "10.2" "11.0" "11.1")
   cudnn=("7" "7" "8" "7" "8" "8" "8")
   for i in ${!cudnn[@]}; do
-    docker push $host/dockerhub/${image}:${CORTEX_VERSION}-cuda${cuda[$i]}-cudnn${cudnn[$i]}
+    docker push $host/cortexlabs/${image}:${CORTEX_VERSION}-cuda${cuda[$i]}-cudnn${cudnn[$i]}
   done
 else
-  docker push $host/dockerhub/${image}:${CORTEX_VERSION}
+  docker push $host/cortexlabs/${image}:${CORTEX_VERSION}
 fi
