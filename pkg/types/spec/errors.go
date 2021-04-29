7952b51f3fc77b470a3c8479f0896ff5cc2acfd8
Robert Lucian Chiriac <robert.lucian.chiriac@gmail.com>
2021-04-30 01:33:50 +0300

    Add shm size option to Task API

--- pkg/types/spec/errors.go
+++ pkg/types/spec/errors.go
@@ -232,10 +232,10 @@ func ErrorSurgeAndUnavailableBothZero() error {
 	})
 }
 
-func ErrorShmSizeCannotExceedMem(shmSize k8s.Quantity, mem k8s.Quantity) error {
+func ErrorShmSizeCannotExceedMem(topSection string, shmSize k8s.Quantity, mem k8s.Quantity) error {
 	return errors.WithStack(&errors.Error{
 		Kind:    ErrShmSizeCannotExceedMem,
-		Message: fmt.Sprintf("handler.shm_size (%s) cannot exceed compute.mem (%s)", shmSize.UserString, mem.UserString),
+		Message: fmt.Sprintf("%s.shm_size (%s) cannot exceed compute.mem (%s)", topSection, shmSize.UserString, mem.UserString),
 	})
 }
 
