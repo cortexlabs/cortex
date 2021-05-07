ec007cbcb2c04d58d59f1715e4230ee1ee4b2dda
Miguel Varela Ramos <miguel@cortexlabs.com>
2021-05-07 15:42:25 +0200

    Add AWS env vars to configure retries (#2147)

--- manager/install.sh
+++ manager/install.sh
@@ -210,6 +210,8 @@ function setup_configmap() {
     --from-literal='CORTEX_VERSION'=$CORTEX_VERSION \
     --from-literal='CORTEX_REGION'=$CORTEX_REGION \
     --from-literal='AWS_DEFAULT_REGION'=$CORTEX_REGION \
+    --from-literal='AWS_RETRY_MODE'="standard" \
+    --from-literal='AWS_MAX_ATTEMPTS'="5" \
     --from-literal='CORTEX_TELEMETRY_DISABLE'=$CORTEX_TELEMETRY_DISABLE \
     --from-literal='CORTEX_TELEMETRY_SENTRY_DSN'=$CORTEX_TELEMETRY_SENTRY_DSN \
     --from-literal='CORTEX_TELEMETRY_SEGMENT_WRITE_KEY'=$CORTEX_TELEMETRY_SEGMENT_WRITE_KEY \
