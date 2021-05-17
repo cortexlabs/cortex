46a3cb37a8e60a51787d033ddee04950371d69b2
Robert Lucian Chiriac <robert.lucian.chiriac@gmail.com>
2021-05-17 15:50:19 +0300

    Fix make operator-local command (#2171)

--- pkg/lib/configreader/reader.go
+++ pkg/lib/configreader/reader.go
@@ -1038,6 +1038,20 @@ func ParseYAMLFile(dest interface{}, validation *StructValidation, filePath stri
 	return nil
 }
 
+func ParseYAMLBytes(dest interface{}, validation *StructValidation, data []byte) error {
+	fileInterface, err := ReadYAMLBytes(data)
+	if err != nil {
+		return nil
+	}
+
+	errs := Struct(dest, fileInterface, validation)
+	if errors.HasError(errs) {
+		return errors.FirstError(errs...)
+	}
+
+	return nil
+}
+
 func ReadYAMLFile(filePath string) (interface{}, error) {
 	fileBytes, err := files.ReadFileBytes(filePath)
 	if err != nil {
