c8379b6e8aed66cd3bd717f8d607998a772f87a6
Miguel Varela Ramos <miguel@cortexlabs.com>
2021-06-09 17:01:42 +0200

    Add AsyncAPI metrics to cortex CLI

--- cli/cmd/lib_async_apis.go
+++ cli/cmd/lib_async_apis.go
@@ -62,6 +62,8 @@ func asyncAPIsTable(asyncAPIs []schema.APIResponse, envNames []string) table.Tab
 
 	var totalFailed int32
 	var totalStale int32
+	var total4XX int
+	var total5XX int
 
 	for i, asyncAPI := range asyncAPIs {
 		lastUpdated := time.Unix(asyncAPI.Spec.LastUpdated, 0)
@@ -74,10 +76,19 @@ func asyncAPIsTable(asyncAPIs []schema.APIResponse, envNames []string) table.Tab
 			asyncAPI.Status.Requested,
 			asyncAPI.Status.Updated.TotalFailed(),
 			libtime.SinceStr(&lastUpdated),
+			latencyStr(asyncAPI.Metrics),
+			code2XXStr(asyncAPI.Metrics),
+			code4XXStr(asyncAPI.Metrics),
+			code5XXStr(asyncAPI.Metrics),
 		})
 
 		totalFailed += asyncAPI.Status.Updated.TotalFailed()
 		totalStale += asyncAPI.Status.Stale.Ready
+
+		if asyncAPI.Metrics.NetworkStats != nil {
+			total4XX += asyncAPI.Metrics.NetworkStats.Code4XX
+			total5XX += asyncAPI.Metrics.NetworkStats.Code5XX
+		}
 	}
 
 	return table.Table{
@@ -90,6 +101,10 @@ func asyncAPIsTable(asyncAPIs []schema.APIResponse, envNames []string) table.Tab
 			{Title: _titleRequested},
 			{Title: _titleFailed, Hidden: totalFailed == 0},
 			{Title: _titleLastupdated},
+			{Title: _titleAvgRequest},
+			{Title: _title2XX},
+			{Title: _title4XX, Hidden: total4XX == 0},
+			{Title: _title5XX, Hidden: total5XX == 0},
 		},
 		Rows: rows,
 	}
