5a022aa42c681af4e761d9817a9c4ba735bb0ae4
Miguel Varela Ramos <miguel@cortexlabs.com>
2021-06-15 12:03:01 +0200

    Add api activator test

---
+++ pkg/activator/api_activator_test.go
@@ -0,0 +1,51 @@
+/*
+Copyright 2021 Cortex Labs, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package activator
+
+import (
+	"context"
+	"testing"
+
+	"github.com/cortexlabs/cortex/pkg/proxy"
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/require"
+)
+
+func TestApiActivator_Try(t *testing.T) {
+	act := newAPIActivator("test", 1, 1)
+
+	errCh := make(chan error)
+	waitCh := make(chan struct{})
+
+	for i := 0; i < 3; i++ {
+		go func() {
+			errCh <- act.Try(context.Background(), func() error {
+				<-waitCh
+				return nil
+			})
+		}()
+	}
+
+	err := <-errCh
+	require.Error(t, err)
+	require.True(t, errors.Is(err, proxy.ErrRequestQueueFull))
+
+	for i := 0; i < 2; i++ {
+		waitCh <- struct{}{}
+		require.NoError(t, <-errCh)
+	}
+}
