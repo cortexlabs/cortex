8e56623c2cf42d9100e98c2c29f0e88b5272618e
Robert Lucian Chiriac <robert.lucian.chiriac@gmail.com>
2021-06-18 00:48:20 +0300

    Use `tfs-cpu`/`web-cpu` instead of `cpu-tfs`/`cpu-web`

--- test/apis/realtime/multi-container/build-tfs-cpu.sh
+++ test/apis/realtime/multi-container/build-tfs-cpu.sh
@@ -3,6 +3,6 @@
 # usage: build-cpu.sh [REGISTRY] [--skip-push]
 #   REGISTRY defaults to $CORTEX_DEV_DEFAULT_IMAGE_REGISTRY; e.g. 764403040460.dkr.ecr.us-west-2.amazonaws.com/cortexlabs or quay.io/cortexlabs-test
 
-image_name="realtime-multi-container-cpu-tfs"
+image_name="realtime-multi-container-tfs-cpu"
 
 "$(dirname "${BASH_SOURCE[0]}")"/../../../utils/build.sh $(realpath "${BASH_SOURCE[0]}") "$image_name" "$@"
