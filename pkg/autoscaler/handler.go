e63ea6464ff5133071e2be4e9fd281a5b27f3ce5
Miguel Varela Ramos <miguel@cortexlabs.com>
2021-06-17 16:28:47 +0200

    Add new autoscaler scaffolding

---
+++ pkg/autoscaler/handler.go
@@ -0,0 +1,65 @@
+/*
+Copyright 2021 Cortex Labs, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package autoscaler
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/cortexlabs/cortex/pkg/lib/errors"
+	"github.com/cortexlabs/cortex/pkg/types/userconfig"
+)
+
+type AutoscalerHandler struct {
+	autoscaler *Autoscaler
+}
+
+func NewHandler(autoscaler *Autoscaler) *AutoscalerHandler {
+	return &AutoscalerHandler{autoscaler: autoscaler}
+}
+
+func (h *AutoscalerHandler) AddAPI(w http.ResponseWriter, r *http.Request) {
+	var api userconfig.Resource
+	if err := json.NewDecoder(r.Body).Decode(&api); err != nil {
+		http.Error(w, "failed to json decode request body", http.StatusBadRequest)
+		return
+	}
+	defer func() { _ = r.Body.Close() }()
+
+	if err := h.autoscaler.AddAPI(api); err != nil {
+		http.Error(w, errors.Wrap(err, "failed to add api to autoscaler").Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func (h *AutoscalerHandler) Awake(w http.ResponseWriter, r *http.Request) {
+	var api userconfig.Resource
+	if err := json.NewDecoder(r.Body).Decode(&api); err != nil {
+		http.Error(w, "failed to json decode request body", http.StatusBadRequest)
+		return
+	}
+	defer func() { _ = r.Body.Close() }()
+
+	if err := h.autoscaler.Awake(api); err != nil {
+		http.Error(w, errors.Wrap(err, "failed to add api to autoscaler").Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
