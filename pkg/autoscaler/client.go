87da45feaeedba2db9095a447d497853aa68921d
Miguel Varela Ramos <miguel@cortexlabs.com>
2021-06-24 16:01:58 +0200

    Add autoscaler client

---
+++ pkg/autoscaler/client.go
@@ -0,0 +1,99 @@
+/*
+Copyright 2021 Cortex Labs, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package autoscaler
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/cortexlabs/cortex/pkg/lib/json"
+	"github.com/cortexlabs/cortex/pkg/lib/urls"
+	"github.com/cortexlabs/cortex/pkg/types/userconfig"
+)
+
+type Client interface {
+	AddAPI(api userconfig.Resource) error
+	Awake(api userconfig.Resource) error
+}
+
+type client struct {
+	httpClient *http.Client
+	endpoint   string
+}
+
+func NewClient(endpoint string) *client {
+	return &client{
+		httpClient: &http.Client{},
+		endpoint:   endpoint,
+	}
+}
+
+func (c *client) AddAPI(api userconfig.Resource) error {
+	payload, err := json.Marshal(api)
+	if err != nil {
+		return err
+	}
+
+	response, err := c.httpClient.Post(
+		urls.Join(c.endpoint, "/add"), "application/json", bytes.NewBuffer(payload),
+	)
+	if err != nil {
+		return err
+	}
+	defer func() { _ = response.Body.Close() }()
+
+	if response.StatusCode != http.StatusOK {
+		bodyBytes, _ := ioutil.ReadAll(response.Body)
+		errMsg := fmt.Sprintf("failed to add api to autoscaler (status code %d)", response.StatusCode)
+		if bodyBytes != nil {
+			errMsg = errMsg + fmt.Sprintf(": %s", string(bodyBytes))
+		}
+
+		return fmt.Errorf(errMsg)
+	}
+
+	return nil
+}
+
+func (c *client) Awake(api userconfig.Resource) error {
+	payload, err := json.Marshal(api)
+	if err != nil {
+		return err
+	}
+
+	response, err := c.httpClient.Post(
+		urls.Join(c.endpoint, "/awake"), "application/json", bytes.NewBuffer(payload),
+	)
+	if err != nil {
+		return err
+	}
+	defer func() { _ = response.Body.Close() }()
+
+	if response.StatusCode != http.StatusOK {
+		bodyBytes, _ := ioutil.ReadAll(response.Body)
+		errMsg := fmt.Sprintf("failed to awake api (status code %d)", response.StatusCode)
+		if bodyBytes != nil {
+			errMsg = errMsg + fmt.Sprintf(": %s", string(bodyBytes))
+		}
+
+		return fmt.Errorf(errMsg)
+	}
+
+	return nil
+}
