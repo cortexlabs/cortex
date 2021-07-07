2a45ce48a5fb25104094ebf3ad694790f00ecdcd
Miguel Varela Ramos <miguel@cortexlabs.com>
2021-07-07 11:03:10 +0200

    Add function to check cluster health

---
+++ pkg/health/health.go
@@ -0,0 +1,167 @@
+/*
+Copyright 2021 Cortex Labs, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package health
+
+import (
+	"context"
+
+	"github.com/cortexlabs/cortex/pkg/lib/json"
+	"github.com/cortexlabs/cortex/pkg/lib/k8s"
+	"github.com/cortexlabs/cortex/pkg/lib/parallel"
+	kapps "k8s.io/api/apps/v1"
+	ctrlclient "sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// ClusterHealth represents the healthiness of each component of a cluster
+type ClusterHealth struct {
+	Operator          bool `json:"operator"`
+	ControllerManager bool `json:"controller_manager"`
+	Prometheus        bool `json:"prometheus"`
+	Autoscaler        bool `json:"autoscaler"`
+	Activator         bool `json:"activator"`
+	Grafana           bool `json:"grafana"`
+	OperatorGateway   bool `json:"operator_gateway"`
+	APIsGateway       bool `json:"apis_gateway"`
+}
+
+func (c ClusterHealth) String() string {
+	bytes, err := json.Marshal(c)
+	if err != nil {
+		panic(err)
+	}
+
+	return string(bytes)
+}
+
+// Check checks for the health of the different components of a cluster
+func Check(client *k8s.Client) (ClusterHealth, error) {
+	var (
+		operatorHealth          bool
+		controllerManagerHealth bool
+		prometheusHealth        bool
+		autoscalerHealth        bool
+		activatorHealth         bool
+		grafanaHealth           bool
+		operatorGatewayHealth   bool
+		apisGatewayHealth       bool
+	)
+
+	ctx := context.Background()
+	if err := parallel.RunFirstErr(
+		func() error {
+			var deployment kapps.Deployment
+			if err := client.Get(ctx, ctrlclient.ObjectKey{
+				Namespace: "default",
+				Name:      "operator",
+			}, &deployment); err != nil {
+				return err
+			}
+			operatorHealth = deployment.Status.ReadyReplicas > 0
+			return nil
+		},
+		func() error {
+			var deployment kapps.Deployment
+			if err := client.Get(ctx, ctrlclient.ObjectKey{
+				Namespace: "default",
+				Name:      "operator-controller-manager",
+			}, &deployment); err != nil {
+				return err
+			}
+			controllerManagerHealth = deployment.Status.ReadyReplicas > 0
+			return nil
+		},
+		func() error {
+			var statefulSet kapps.StatefulSet
+			if err := client.Get(ctx, ctrlclient.ObjectKey{
+				Namespace: "default",
+				Name:      "prometheus-prometheus",
+			}, &statefulSet); err != nil {
+				return err
+			}
+			prometheusHealth = statefulSet.Status.ReadyReplicas > 0
+			return nil
+		},
+		func() error {
+			var deployment kapps.Deployment
+			if err := client.Get(ctx, ctrlclient.ObjectKey{
+				Namespace: "default",
+				Name:      "autoscaler",
+			}, &deployment); err != nil {
+				return err
+			}
+			autoscalerHealth = deployment.Status.ReadyReplicas > 0
+			return nil
+		},
+		func() error {
+			var deployment kapps.Deployment
+			if err := client.Get(ctx, ctrlclient.ObjectKey{
+				Namespace: "default",
+				Name:      "activator",
+			}, &deployment); err != nil {
+				return err
+			}
+			activatorHealth = deployment.Status.ReadyReplicas > 0
+			return nil
+		},
+		func() error {
+			var statefulSet kapps.StatefulSet
+			if err := client.Get(ctx, ctrlclient.ObjectKey{
+				Namespace: "default",
+				Name:      "grafana",
+			}, &statefulSet); err != nil {
+				return err
+			}
+			grafanaHealth = statefulSet.Status.ReadyReplicas > 0
+			return nil
+		},
+		func() error {
+			var deployment kapps.Deployment
+			if err := client.Get(ctx, ctrlclient.ObjectKey{
+				Namespace: "istio-system",
+				Name:      "ingressgateway-operator",
+			}, &deployment); err != nil {
+				return err
+			}
+			operatorGatewayHealth = deployment.Status.ReadyReplicas > 0
+			return nil
+		},
+		func() error {
+			var deployment kapps.Deployment
+			if err := client.Get(ctx, ctrlclient.ObjectKey{
+				Namespace: "istio-system",
+				Name:      "ingressgateway-apis",
+			}, &deployment); err != nil {
+				return err
+			}
+			apisGatewayHealth = deployment.Status.ReadyReplicas > 0
+			return nil
+		},
+	); err != nil {
+		return ClusterHealth{}, err
+	}
+
+	return ClusterHealth{
+		Operator:          operatorHealth,
+		ControllerManager: controllerManagerHealth,
+		Prometheus:        prometheusHealth,
+		Autoscaler:        autoscalerHealth,
+		Activator:         activatorHealth,
+		Grafana:           grafanaHealth,
+		OperatorGateway:   operatorGatewayHealth,
+		APIsGateway:       apisGatewayHealth,
+	}, nil
+}
