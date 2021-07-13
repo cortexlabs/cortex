92a6c8aa0b7fdf0128bb095e67064de9e3b0bc78
Miguel Varela Ramos <miguel@cortexlabs.com>
2021-07-13 13:42:20 +0200

    Add tests to check if autoscaler respects min and max replicas

--- pkg/autoscaler/autoscaler_test.go
+++ pkg/autoscaler/autoscaler_test.go
@@ -42,6 +42,7 @@ func newLogger(t *testing.T) *zap.SugaredLogger {
 }
 
 func TestAutoscaler_Awake(t *testing.T) {
+	t.Parallel()
 	log := newLogger(t)
 
 	mux := sync.RWMutex{}
@@ -112,4 +113,142 @@ func TestAutoscaler_Awake(t *testing.T) {
 		defer mux.RUnlock()
 		return latestRequest != 1
 	}, downscaleStabilizationPeriod, time.Second)
+}
+
+func TestAutoscaler_MinReplicas(t *testing.T) {
+	t.Parallel()
+	log := newLogger(t)
+
+	mux := sync.RWMutex{}
+	var latestRequest int32
+
+	minReplicas := int32(5)
+	maxReplicas := int32(10)
+
+	scalerMock := &ScalerFunc{
+		ScaleFunc: func(apiName string, request int32) error {
+			mux.Lock()
+			defer mux.Unlock()
+
+			latestRequest = request
+			return nil
+		},
+		GetInFlightRequestsFunc: func(apiName string, window time.Duration) (*float64, error) {
+			return pointer.Float64(0), nil
+		},
+		GetAutoscalingSpecFunc: func(apiName string) (*userconfig.Autoscaling, error) {
+			return &userconfig.Autoscaling{
+				MinReplicas:    minReplicas,
+				MaxReplicas:    maxReplicas,
+				InitReplicas:   minReplicas,
+				TargetInFlight: pointer.Float64(1),
+				Window:         time.Second,
+			}, nil
+		},
+		CurrentReplicasFunc: func(apiName string) (int32, error) {
+			return minReplicas + 1, nil
+		},
+	}
+
+	autoScaler := &Autoscaler{
+		logger:              log,
+		crons:               make(map[string]cron.Cron),
+		scalers:             make(map[userconfig.Kind]Scaler),
+		lastAwakenTimestamp: make(map[string]time.Time),
+	}
+	autoScaler.AddScaler(scalerMock, userconfig.RealtimeAPIKind)
+
+	api := userconfig.Resource{
+		Name: "test",
+		Kind: userconfig.RealtimeAPIKind,
+	}
+
+	autoscaleFn, err := autoScaler.autoscaleFn(api)
+	require.NoError(t, err)
+
+	ticker := time.NewTicker(500 * time.Millisecond)
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				err := autoscaleFn()
+				require.NoError(t, err)
+			}
+		}
+	}()
+
+	require.Never(t, func() bool {
+		mux.RLock()
+		defer mux.RUnlock()
+		return latestRequest < minReplicas
+	}, 3*time.Second, time.Second)
+}
+
+func TestAutoscaler_MaxReplicas(t *testing.T) {
+	t.Parallel()
+	log := newLogger(t)
+
+	mux := sync.RWMutex{}
+	var latestRequest int32
+
+	minReplicas := int32(5)
+	maxReplicas := int32(10)
+
+	scalerMock := &ScalerFunc{
+		ScaleFunc: func(apiName string, request int32) error {
+			mux.Lock()
+			defer mux.Unlock()
+
+			latestRequest = request
+			return nil
+		},
+		GetInFlightRequestsFunc: func(apiName string, window time.Duration) (*float64, error) {
+			return pointer.Float64(20), nil
+		},
+		GetAutoscalingSpecFunc: func(apiName string) (*userconfig.Autoscaling, error) {
+			return &userconfig.Autoscaling{
+				MinReplicas:    minReplicas,
+				MaxReplicas:    maxReplicas,
+				InitReplicas:   minReplicas,
+				TargetInFlight: pointer.Float64(1),
+				Window:         time.Second,
+			}, nil
+		},
+		CurrentReplicasFunc: func(apiName string) (int32, error) {
+			return minReplicas, nil
+		},
+	}
+
+	autoScaler := &Autoscaler{
+		logger:              log,
+		crons:               make(map[string]cron.Cron),
+		scalers:             make(map[userconfig.Kind]Scaler),
+		lastAwakenTimestamp: make(map[string]time.Time),
+	}
+	autoScaler.AddScaler(scalerMock, userconfig.RealtimeAPIKind)
+
+	api := userconfig.Resource{
+		Name: "test",
+		Kind: userconfig.RealtimeAPIKind,
+	}
+
+	autoscaleFn, err := autoScaler.autoscaleFn(api)
+	require.NoError(t, err)
+
+	ticker := time.NewTicker(500 * time.Millisecond)
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				err := autoscaleFn()
+				require.NoError(t, err)
+			}
+		}
+	}()
+
+	require.Never(t, func() bool {
+		mux.RLock()
+		defer mux.RUnlock()
+		return latestRequest > maxReplicas
+	}, 3*time.Second, time.Second)
 }
