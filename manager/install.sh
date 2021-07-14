6485723d5fc60a322c46915f77db9d910b77b35c
Robert Lucian Chiriac <robert.lucian.chiriac@gmail.com>
2021-07-14 22:37:30 +0300

    Limit istiod to 5 replicas and the default namespace

--- manager/install.sh
+++ manager/install.sh
@@ -361,6 +361,7 @@ function remove_nodegroups() {
 
 function setup_istio() {
   envsubst < manifests/istio-namespace.yaml | kubectl apply -f - >/dev/null
+  kubectl label namespaces default istio-discovery=enabled
 
   if ! grep -q "istio-customgateway-certs" <<< $(kubectl get secret -n istio-system); then
     WEBSITE=localhost
