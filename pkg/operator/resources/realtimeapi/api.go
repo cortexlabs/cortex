128cf5b24d47f98fa98e13853ed49e4ee558b79d
Miguel Varela Ramos <miguel@cortexlabs.com>
2021-07-29 14:31:28 +0200

    Fix deep equal comparison

--- pkg/operator/resources/realtimeapi/api.go
+++ pkg/operator/resources/realtimeapi/api.go
@@ -19,8 +19,6 @@ package realtimeapi
 import (
 	"context"
 	"fmt"
-	"path/filepath"
-	"reflect"
 	"sort"
 	"time"
 
@@ -28,7 +26,6 @@ import (
 	"github.com/cortexlabs/cortex/pkg/consts"
 	serverless "github.com/cortexlabs/cortex/pkg/crds/apis/serverless/v1alpha1"
 	"github.com/cortexlabs/cortex/pkg/lib/errors"
-	"github.com/cortexlabs/cortex/pkg/lib/k8s"
 	"github.com/cortexlabs/cortex/pkg/lib/maps"
 	"github.com/cortexlabs/cortex/pkg/lib/parallel"
 	"github.com/cortexlabs/cortex/pkg/lib/pointer"
@@ -38,21 +35,13 @@ import (
 	"github.com/cortexlabs/cortex/pkg/types/spec"
 	"github.com/cortexlabs/cortex/pkg/types/status"
 	"github.com/cortexlabs/cortex/pkg/types/userconfig"
-	"github.com/cortexlabs/cortex/pkg/workloads"
+	"github.com/google/go-cmp/cmp"
 	kcore "k8s.io/api/core/v1"
 	kerrors "k8s.io/apimachinery/pkg/api/errors"
-	kresource "k8s.io/apimachinery/pkg/api/resource"
 	kmeta "k8s.io/apimachinery/pkg/apis/meta/v1"
 	ktypes "k8s.io/apimachinery/pkg/types"
-	"k8s.io/apimachinery/pkg/util/intstr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
-
-const _realtimeDashboardUID = "realtimeapi"
-
-func generateDeploymentID() string {
-	return k8s.RandomName()[:10]
-}
 
 func UpdateAPI(apiConfig *userconfig.API, force bool) (*spec.API, string, error) {
 	ctx := context.Background()
@@ -103,7 +92,7 @@ func UpdateAPI(apiConfig *userconfig.API, force bool) (*spec.API, string, error)
 		MetadataRoot:          spec.MetadataRoot(apiConfig.Name, config.ClusterConfig.ClusterUID),
 	}
 
-	if !reflect.DeepEqual(api.Spec, desiredAPI.Spec) || force {
+	if !cmp.Equal(api.Spec, desiredAPI.Spec) || force {
 		api.Spec = desiredAPI.Spec
 		api.Annotations = maps.MergeStrMapsString(api.Annotations, desiredAPI.Annotations)
 
@@ -189,7 +178,7 @@ func GetAllAPIs() ([]schema.APIResponse, error) {
 	}
 
 	realtimeAPIs := make([]schema.APIResponse, len(apis.Items))
-	mappedRealtimeAPIs := make(map[string]schema.APIResponse, 0)
+	mappedRealtimeAPIs := map[string]schema.APIResponse{}
 	for i := range apis.Items {
 		api := apis.Items[i]
 
@@ -288,203 +277,4 @@ func DescribeAPIByName(apiName string) ([]schema.APIResponse, error) {
 			DashboardURL:  dashboardURL,
 		},
 	}, nil
-}
-
-func getDashboardURL(apiName string) string {
-	loadBalancerURL, err := operator.LoadBalancerURL()
-	if err != nil {
-		return ""
-	}
-
-	dashboardURL := fmt.Sprintf(
-		"%s/dashboard/d/%s/realtimeapi?orgId=1&refresh=30s&var-api_name=%s",
-		loadBalancerURL, _realtimeDashboardUID, apiName,
-	)
-
-	return dashboardURL
-}
-
-// K8sResourceFromAPIConfig converts a cortex API config into a realtime API CRD resource
-func K8sResourceFromAPIConfig(apiConfig userconfig.API) serverless.RealtimeAPI {
-	containers := make([]serverless.ContainerSpec, len(apiConfig.Pod.Containers))
-	for i := range apiConfig.Pod.Containers {
-		container := apiConfig.Pod.Containers[i]
-		var env []kcore.EnvVar
-		for k, v := range container.Env {
-			env = append(env, kcore.EnvVar{
-				Name:  k,
-				Value: v,
-			})
-		}
-
-		var compute *serverless.ComputeSpec
-		if container.Compute != nil {
-			var cpu *kresource.Quantity
-			if container.Compute.CPU != nil {
-				cpu = &container.Compute.CPU.Quantity
-			}
-			var mem *kresource.Quantity
-			if container.Compute.Mem != nil {
-				mem = &container.Compute.Mem.Quantity
-			}
-			var shm *kresource.Quantity
-			if container.Compute.Shm != nil {
-				shm = &container.Compute.Shm.Quantity
-			}
-
-			compute = &serverless.ComputeSpec{
-				CPU: cpu,
-				GPU: container.Compute.GPU,
-				Inf: container.Compute.Inf,
-				Mem: mem,
-				Shm: shm,
-			}
-		}
-
-		containers[i] = serverless.ContainerSpec{
-			Name:           container.Name,
-			Image:          container.Image,
-			Command:        container.Command,
-			Args:           container.Args,
-			Env:            env,
-			Compute:        compute,
-			ReadinessProbe: workloads.GetProbeSpec(container.ReadinessProbe),
-			LivenessProbe:  workloads.GetProbeSpec(container.LivenessProbe),
-		}
-	}
-
-	api := serverless.RealtimeAPI{
-		ObjectMeta: kmeta.ObjectMeta{
-			Name:      apiConfig.Name,
-			Namespace: consts.DefaultNamespace,
-		},
-		Spec: serverless.RealtimeAPISpec{
-			Pod: serverless.PodSpec{
-				Port:           *apiConfig.Pod.Port,
-				MaxConcurrency: int32(apiConfig.Pod.MaxConcurrency),
-				MaxQueueLength: int32(apiConfig.Pod.MaxQueueLength),
-				Replicas:       apiConfig.Autoscaling.InitReplicas,
-				Containers:     containers,
-			},
-			Autoscaling: serverless.AutoscalingSpec{
-				MinReplicas:                  apiConfig.Autoscaling.MinReplicas,
-				MaxReplicas:                  apiConfig.Autoscaling.MaxReplicas,
-				TargetInFlight:               fmt.Sprintf("%f", *apiConfig.Autoscaling.TargetInFlight),
-				Window:                       kmeta.Duration{Duration: apiConfig.Autoscaling.Window},
-				DownscaleStabilizationPeriod: kmeta.Duration{Duration: apiConfig.Autoscaling.DownscaleStabilizationPeriod},
-				UpscaleStabilizationPeriod:   kmeta.Duration{Duration: apiConfig.Autoscaling.UpscaleStabilizationPeriod},
-				MaxDownscaleFactor:           fmt.Sprintf("%f", apiConfig.Autoscaling.MaxDownscaleFactor),
-				MaxUpscaleFactor:             fmt.Sprintf("%f", apiConfig.Autoscaling.MaxUpscaleFactor),
-				DownscaleTolerance:           fmt.Sprintf("%f", apiConfig.Autoscaling.DownscaleTolerance),
-				UpscaleTolerance:             fmt.Sprintf("%f", apiConfig.Autoscaling.UpscaleTolerance),
-			},
-			NodeGroups: apiConfig.NodeGroups,
-			UpdateStrategy: serverless.UpdateStrategySpec{
-				MaxSurge:       intstr.FromString(apiConfig.UpdateStrategy.MaxSurge),
-				MaxUnavailable: intstr.FromString(apiConfig.UpdateStrategy.MaxUnavailable),
-			},
-			Networking: serverless.NetworkingSpec{
-				Endpoint: *apiConfig.Networking.Endpoint,
-			},
-		},
-	}
-
-	deploymentID, podID, specID, apiID := api.GetOrCreateAPIIDs()
-	api.Annotations = map[string]string{
-		"cortex.dev/deployment-id": deploymentID,
-		"cortex.dev/spec-id":       specID,
-		"cortex.dev/pod-id":        podID,
-		"cortex.dev/api-id":        apiID,
-	}
-
-	return api
-}
-
-func deleteBucketResources(apiName string) error {
-	prefix := filepath.Join(config.ClusterConfig.ClusterUID, "apis", apiName)
-	return config.AWS.DeleteS3Dir(config.ClusterConfig.Bucket, prefix, true)
-}
-
-func metadataFromRealtimeAPI(sv *serverless.RealtimeAPI) (*spec.Metadata, error) {
-	lastUpdated, err := spec.TimeFromAPIID(sv.Annotations["cortex.dev/api-id"])
-	if err != nil {
-		return nil, err
-	}
-	return &spec.Metadata{
-		Resource: &userconfig.Resource{
-			Name: sv.Name,
-			Kind: userconfig.RealtimeAPIKind,
-		},
-		APIID:        sv.Annotations["cortex.dev/api-id"],
-		DeploymentID: sv.Annotations["cortex.dev/deployment-id"],
-		LastUpdated:  lastUpdated.Unix(),
-	}, nil
-}
-
-func getReplicaCounts(pods []kcore.Pod, metadata *spec.Metadata) status.ReplicaCounts {
-	counts := status.ReplicaCounts{}
-
-	for i := range pods {
-		pod := pods[i]
-		if pod.Labels["apiName"] != metadata.Name {
-			continue
-		}
-		addPodToReplicaCounts(&pods[i], metadata, &counts)
-	}
-
-	return counts
-}
-
-func addPodToReplicaCounts(pod *kcore.Pod, metadata *spec.Metadata, counts *status.ReplicaCounts) {
-	latest := false
-	if isPodSpecLatest(pod, metadata) {
-		latest = true
-	}
-
-	isPodReady := k8s.IsPodReady(pod)
-	if latest && isPodReady {
-		counts.Ready++
-		return
-	} else if !latest && isPodReady {
-		counts.ReadyOutOfDate++
-		return
-	}
-
-	podStatus := k8s.GetPodStatus(pod)
-
-	if podStatus == k8s.PodStatusTerminating {
-		counts.Terminating++
-		return
-	}
-
-	if !latest {
-		return
-	}
-
-	switch podStatus {
-	case k8s.PodStatusPending:
-		counts.Pending++
-	case k8s.PodStatusStalled:
-		counts.Stalled++
-	case k8s.PodStatusCreating:
-		counts.Creating++
-	case k8s.PodStatusReady:
-		counts.Ready++
-	case k8s.PodStatusNotReady:
-		counts.NotReady++
-	case k8s.PodStatusErrImagePull:
-		counts.ErrImagePull++
-	case k8s.PodStatusFailed:
-		counts.Failed++
-	case k8s.PodStatusKilled:
-		counts.Killed++
-	case k8s.PodStatusKilledOOM:
-		counts.KilledOOM++
-	case k8s.PodStatusUnknown:
-		counts.Unknown++
-	}
-}
-
-func isPodSpecLatest(pod *kcore.Pod, metadata *spec.Metadata) bool {
-	return metadata.APIID == pod.Labels["apiID"]
 }
