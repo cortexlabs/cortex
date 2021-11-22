2fd30e4cc7e26ed6a86979cc18fb75f13878595b
David Eliahu <deliahu@users.noreply.github.com>
2021-11-22 11:12:21 -0800

    Add get_operator_url.py

---
+++ dev/get_operator_url.py
@@ -0,0 +1,39 @@
+import sys
+import boto3
+
+
+def main():
+    cluster_name = sys.argv[1]
+    region = sys.argv[2]
+    operator_url = get_operator_url(cluster_name, region)
+    print("https://" + operator_url)
+
+
+def get_operator_url(cluster_name, region):
+    client_elbv2 = boto3.client("elbv2", region_name=region)
+
+    paginator = client_elbv2.get_paginator("describe_load_balancers")
+    for load_balancer_page in paginator.paginate(PaginationConfig={"PageSize": 20}):
+        load_balancers = {
+            load_balancer["LoadBalancerArn"]: load_balancer
+            for load_balancer in load_balancer_page["LoadBalancers"]
+        }
+        tag_descriptions = client_elbv2.describe_tags(ResourceArns=list(load_balancers.keys()))[
+            "TagDescriptions"
+        ]
+        for tag_description in tag_descriptions:
+            foundClusterNameTag = False
+            foundLoadBalancerTag = False
+            for tags in tag_description["Tags"]:
+                if tags["Key"] == "cortex.dev/cluster-name" and tags["Value"] == cluster_name:
+                    foundClusterNameTag = True
+                if tags["Key"] == "cortex.dev/load-balancer" and tags["Value"] == "operator":
+                    foundLoadBalancerTag = True
+            if foundClusterNameTag and foundLoadBalancerTag:
+                load_balancer = load_balancers[tag_description["ResourceArn"]]
+                return load_balancer["DNSName"]
+
+
+# usage: python get_operator_url.py CLUSTER_NAME REGION
+if __name__ == "__main__":
+    main()
